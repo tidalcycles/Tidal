cbfd68dedf6455510b1f1bc30b539929c6d31457
Alex <alex@slab.org>
2013-09-27 10:49:01 +0100

    add trunc, whenmod, triple and quad. rename double to superimpose and make double a synonym for echo

--- Strategies.hs
+++ Strategies.hs
@@ -10,9 +10,14 @@ import Control.Applicative
 
 import Utils
 
-echo n p = stack [p, n ~> p]
+stutter n t p = stack $ map (\i -> (t * (fromIntegral i)) ~> p) [0 .. (n-1)]
 
-double f p = stack [p, f p]
+echo   = stutter 2
+triple = stutter 3
+quad   = stutter 4
+double = echo
+
+superimpose f p = stack [p, f p]
 
 -- every 4 (smash 4 [1, 2, 3]) $ sound "[odx sn/2 [~ odx] sn/3, [~ hh]*4]"
 
@@ -37,6 +42,7 @@ spread' f timepat pat =
   Pattern $ \r -> concatMap (\(r', x) -> (arc (f x pat) r')) (rs r)
   where rs r = arc (filterOffsets timepat) r
 
+{-
 scrumple :: Time -> Pattern a -> Pattern a -> Pattern a
 scrumple o p p' = p'' -- overlay p (o ~> p'')
   where p'' = Pattern $ \a -> concatMap 
@@ -46,5 +52,26 @@ scrumple o p p' = p'' -- overlay p (o ~> p'')
                                                    )
                                                    (arc p' (s,s))
                               ) (arc p a)
+-}
 
+whenmod :: Int -> Int -> (Pattern a -> Pattern a) -> Pattern a -> Pattern a
 whenmod a b = Pattern.when ((\t -> (t `mod` a) >= b ))
+
+--rev :: Pattern a -> Pattern a
+--rev p = Pattern $ \a -> concatMap 
+--                        (\a' -> mapFsts mirrorArc $ 
+--                                (arc p (mirrorArc a')))
+--                        (arcCycles a)
+
+trunc :: Time -> Pattern a -> Pattern a
+trunc t p = slow t $ Pattern $ \a -> concatMap f $ arcCycles a
+  where f a = mapFsts (stretch . trunc') $ arc p (trunc' a)
+        trunc' (s,e) = (min s ((sam s) + t), min e ((sam s) + t))
+        stretch (s,e) = (sam s + ((s - sam s) / t), sam s + ((e - sam s) / t))
+
+{-stripe :: Arc -> Pattern a -> Pattern a
+stripe (stripeS, stripeE) p = slow t $ Pattern $ \a -> concatMap f $ arcCycles a
+  where f a = mapFsts (stretch . stripe') $ arc p (stripe' a)
+        trunc' (s,e) = (min s ((sam s) + t), min e ((sam s) + t))
+        stretch (s,e) = (sam s + ((s - sam s) / t), sam s + ((e - sam s) / t))
+-}
