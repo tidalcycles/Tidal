e135f89c3508e499043609b6d1da205cbd8d3417
alex <alex@slab.org>
2013-11-01 11:06:58 +0000

    new file

---
+++ Sound/Tidal/Context.hs
@@ -0,0 +1,12 @@
+module Sound.Tidal.Context (module C) where
+
+import Control.Concurrent as C
+import Data.List as C
+import Control.Applicative as C
+import Data.Ratio as C
+import Sound.Tidal.Parse as C
+import Sound.Tidal.Pattern as C
+import Sound.Tidal.Stream as C
+import Sound.Tidal.Dirt as C
+import Sound.Tidal.Strategies as C
+import Sound.Tidal.Tempo as C
