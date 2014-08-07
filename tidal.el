3a6d7b8ebdcf2695e0e8cb36e4454465feb51450
= <=>
2014-08-07 20:07:47 +0100

    add fadeIn and fadeOut functions

--- tidal.el
+++ tidal.el
@@ -136,6 +136,9 @@
   (tidal-send-string "now' <- getNow")
   (tidal-send-string "let now = nextSam now'")
   (tidal-send-string "let retrig = (now ~>)")
+  (tidal-send-string "let fadeOut n = spread' (degradeBy) (retrig $ slow n $ envL)")
+  (tidal-send-string "let fadeIn n = spread' (degradeBy) (retrig $ slow n $ (1-) <$> envL)")
+
   )
 
 (defun tidal-run-line ()
