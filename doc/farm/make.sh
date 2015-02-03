0c6a2f4eae6ccb6671eca89c56d7257e6c121ff9
alex <alex@slab.org>
2015-02-03 20:12:05 +0000

    fix mike's email

--- doc/farm/make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./process_examples.pl
-#cp farm.tex farm_processed.tex
-pdflatex farm_processed && bibtex farm_processed && xelatex farm_processed && xelatex farm_processed
-mv farm_processed.pdf farm.pdf
-
