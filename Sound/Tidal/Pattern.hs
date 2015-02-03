064220ff2e51f5b8bd28c2252ee97840a84af6a1
Mike Hodnick <mike.hodnick@gmail.com>
2015-02-03 13:13:49 -0600

    renamed every' to foldEvery

--- Sound/Tidal/Pattern.hs
+++ Sound/Tidal/Pattern.hs
@@ -260,12 +260,10 @@ every :: Int -> (Pattern a -> Pattern a) -> Pattern a -> Pattern a
 every 0 f p = p
 every n f p = when ((== 0) . (`mod` n)) f p
 
--- | @every' ns f p@ applies the function @f@ to @p@, and is applied for
+-- | @foldEvery ns f p@ applies the function @f@ to @p@, and is applied for
 -- each cycle in @ns@.
-every' :: [Int] -> (Pattern a -> Pattern a) -> Pattern a -> Pattern a
-every' ns f p = foldr ($) p (map (\x -> every x f) ns)
-
-
+foldEvery :: [Int] -> (Pattern a -> Pattern a) -> Pattern a -> Pattern a
+foldEvery ns f p = foldr ($) p (map (\x -> every x f) ns)
 
 -- | @sig f@ takes a function from time to values, and turns it into a
 -- @Pattern@.
