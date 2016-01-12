1e9590c1f41353b2d373a77e012630371c8b9937
alex <alex@slab.ogr>
2016-01-12 10:01:58 +0000

    allow sound to be specified as separate `s` and `n` parameters. Also tidy up nudge a bit

--- Sound/Tidal/OscStream.hs
+++ Sound/Tidal/OscStream.hs
@@ -8,6 +8,7 @@ import Sound.Tidal.Utils
 import GHC.Float (float2Double, double2Float)
 import Sound.OSC.FD
 import Sound.OSC.Datum
+import Sound.Tidal.Params
 
 data TimeStamp = BundleStamp | MessageStamp | NoStamp
  deriving Eq
@@ -39,7 +40,7 @@ send s slang shape change tick (o, m) = osc
           | otherwise = doAt logicalOnset $ sendOSC s $ Message (path slang) oscdata
       oscPreamble = cpsPrefix ++ preamble slang
       oscdata | namedParams slang = oscPreamble ++ (concatMap (\(k, Just v) -> [string (name k), v] )
-                                                    $ filter (isJust . snd) $ Map.assocs m)
+                                                    $ filter (isJust . snd) $ Map.assocs (fixSound m))
               | otherwise = oscPreamble ++ (catMaybes $ mapMaybe (\x -> Map.lookup x m) (params shape))
       cpsPrefix | cpsStamp shape = [float (cps change)]
                 | otherwise = []
@@ -47,10 +48,22 @@ send s slang shape change tick (o, m) = osc
       usec = floor $ 1000000 * (logicalOnset - (fromIntegral sec))
       sec = floor logicalOnset
       logicalOnset = logicalOnset' change tick o ((latency shape) + nudge)
-      nudge = maybe 0 (toF) (Map.lookup (F "nudge" (Just 0)) m)
+      nudge = maybe 0 (toF) (Map.lookup nudge_p m)
       toF (Just (Float f)) = float2Double f
       toF _ = 0
+      fixSound :: OscMap -> OscMap
+      fixSound m | Map.lookup sound_p m == Nothing = makeSound m
+                 | otherwise = m
+      makeSound :: OscMap -> OscMap
+      makeSound m | s' == Nothing = m
+                  | n' == Nothing = Map.union m (Map.singleton sound_p (fromJust s'))
+                  -- omg
+                  | otherwise = Map.union m (Map.singleton sound_p (Just $ string $ (fromJust $ datum_string $ fromJust $ fromJust s') ++ (':':(show $ fromJust $ datum_int32 $ fromJust $ fromJust n'))))
+        where s' = Map.lookup s_p m
+              n' = Map.lookup n_p m
 
+-- type OscMap = Map.Map Param (Maybe Datum)
+              
 -- Returns a function that will convert a generic ParamMap into a specific Osc message and send it over UDP to the supplied server
 -- messages will be built according to the given OscSlang
 makeConnection :: String -> Int -> OscSlang -> IO (ToMessageFunc)
