529d68c908b100a2f9053fcd70f3e7ca39b4bae0
Julian Rohrhuber <julian.rohrhuber@musikundmedien.net>
2016-01-19 00:10:26 +0100

    add reverb effect parameters

--- Sound/Tidal/Params.hs
+++ Sound/Tidal/Params.hs
@@ -129,3 +129,11 @@ loop_p = I "loop" (Just 1)
 channel :: Pattern Int -> ParamPattern
 channel = make' VI channel_p
 channel_p = I "channel" Nothing
+
+bandq :: Pattern Double -> ParamPattern
+room = make' VF room_p
+room_p = F "room" Nothing
+
+bandq :: Pattern Double -> ParamPattern
+size = make' VF size_p
+size_p = F "size" Nothing
