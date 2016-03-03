e8ea15397f9b19dace4d440c83bc50655a4655ad
Lennart Melzer <lennart@melzer.it>
2016-03-03 23:51:16 +0100

    [++] `superimpose` docs

--- Sound/Tidal/Pattern.hs
+++ Sound/Tidal/Pattern.hs
@@ -494,6 +494,22 @@ wedge t p p' = overlay (densityGap (1/t) p) (t ~> densityGap (1/(1-t)) p')
 whenmod :: Int -> Int -> (Pattern a -> Pattern a) -> Pattern a -> Pattern a
 whenmod a b = Sound.Tidal.Pattern.when ((\t -> (t `mod` a) >= b ))
 
+{- | ~~~~{haskell}
+
+superimpose f p = stack [p, f p]
+
+~~~~
+
+`superimpose` plays a modified version of a pattern at the same time as the original pattern,
+resulting in two patterns being played at the same time.
+
+~~~~{haskell}
+
+d1 $ superimpose (density 2) $ sound "bd sn [cp ht] hh"
+d1 $ superimpose ((# speed "2") . (0.125 <~)) $ sound "bd sn cp hh"
+
+~~~~
+-}
 superimpose f p = stack [p, f p]
 
 -- | @splitQueries p@ wraps `p` to ensure that it does not get
