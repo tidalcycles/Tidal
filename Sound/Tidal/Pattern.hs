11d095e072b94fa300b3c0cc34536a6bd70ffeff
Lennart Melzer <lennart@melzer.it>
2016-03-05 18:59:39 +0100

    [++] `whenmod` docs

--- Sound/Tidal/Pattern.hs
+++ Sound/Tidal/Pattern.hs
@@ -698,6 +698,18 @@ degrade = degradeBy 0.5
 wedge :: Time -> Pattern a -> Pattern a -> Pattern a
 wedge t p p' = overlay (densityGap (1/t) p) (t ~> densityGap (1/(1-t)) p')
 
+{- | `whenmod` has a similar form and behavior to `every`, but requires an 
+additional number. Applies the function to the pattern, when the
+remainder of the current loop number divided by the first parameter,
+is less than the second parameter.
+
+For example the following makes every other block of four loops twice
+as dense:
+
+@
+d1 $ whenmod 8 4 (density 2) (sound "bd sn kurt")
+@
+-}
 whenmod :: Int -> Int -> (Pattern a -> Pattern a) -> Pattern a -> Pattern a
 whenmod a b = Sound.Tidal.Pattern.when ((\t -> (t `mod` a) >= b ))
 
