023693100f18c48e0e582a1d8577d1ed911fbab8
Lennart Melzer <lennart@melzer.it>
2016-03-05 18:59:53 +0100

    [++] `trunc` docs

--- Sound/Tidal/Pattern.hs
+++ Sound/Tidal/Pattern.hs
@@ -737,6 +737,13 @@ superimpose f p = stack [p, f p]
 splitQueries :: Pattern a -> Pattern a
 splitQueries p = Pattern $ \a -> concatMap (arc p) $ arcCycles a
 
+{- | Truncates a pattern so that only a fraction of the pattern is played. 
+The following example plays only the first three quarters of the pattern:
+
+@
+d1 $ trunc 0.75 $ sound "bd sn*2 cp hh*4 arpy bd*2 cp bd*2"
+@
+-}
 trunc :: Time -> Pattern a -> Pattern a
 trunc t p = slow t $ splitQueries $ p'
   where p' = Pattern $ \a -> mapArcs (stretch . trunc') $ arc p (trunc' a)
