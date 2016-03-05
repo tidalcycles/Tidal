27ff2fb595d9523d0242c637c893cd523ad7c30b
Lennart Melzer <lennart@melzer.it>
2016-03-05 18:58:10 +0100

    [++] `slowspread` docs

--- Sound/Tidal/Pattern.hs
+++ Sound/Tidal/Pattern.hs
@@ -478,6 +478,26 @@ d1 $ spread slow [2,4%3] $ sound "ho ho:2 ho:3 hc"
 spread :: (a -> t -> Pattern b) -> [a] -> t -> Pattern b
 spread f xs p = cat $ map (\x -> f x p) xs
 
+{- | `slowspread` takes a list of pattern transforms and applies them one at a time, per cycle, 
+then repeats.
+
+Example:
+
+@
+d1 $ slowspread ($) [density 2, rev, slow 2, striate 3, (# speed "0.8")] 
+    $ sound "[bd*2 [~ bd]] [sn future]*2 cp jvbass*4"
+@
+
+Above, the pattern will have these transforms applied to it, one at a time, per cycle:
+
+* cycle 1: `density 2` - pattern will increase in speed
+* cycle 2: `rev` - pattern will be reversed
+* cycle 3: `slow 2` - pattern will decrease in speed
+* cycle 4: `striate 3` - pattern will be granualized
+* cycle 5: `(# speed "0.8")` - pattern samples will be played back more slowly
+
+After `(# speed "0.8")`, the transforms will repeat and start at `density 2` again.
+-}
 slowspread :: (a -> t -> Pattern b) -> [a] -> t -> Pattern b
 slowspread f xs p = slowcat $ map (\x -> f x p) xs
 
