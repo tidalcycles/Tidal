e68dfe7fb0780fb2e870c82e8effadc2c4c04f5d
Lennart Melzer <lennart@melzer.it>
2016-03-05 19:01:53 +0100

    [++] `smash` and `smash'` docs

--- Sound/Tidal/Strategies.hs
+++ Sound/Tidal/Strategies.hs
@@ -97,11 +97,35 @@ and 0.75, rather than 0 and 1.
 -}
 juxBy n f p = stack [p # pan (pure $ 0.5 - (n/2)), f $ p # pan (pure $ 0.5 + (n/2))]
 
--- every 4 (smash 4 [1, 2, 3]) $ sound "[odx sn/2 [~ odx] sn/3, [~ hh]*4]"
+{- | Smash is a combination of `spread` and `striate` - it cuts the samples
+into the given number of bits, and then cuts between playing the loop
+at different speeds according to the values in the list.
 
+So this:
+
+@
+d1 $ smash 3 [2,3,4] $ sound "ho ho:2 ho:3 hc"
+@
+
+Is a bit like this:
+
+@
+d1 $ spread (slow) [2,3,4] $ striate 3 $ sound "ho ho:2 ho:3 hc"
+@
+
+This is quite dancehall:
+
+@
+d1 $ (spread' slow "1%4 2 1 3" $ spread (striate) [2,3,4,1] $ sound
+"sn:2 sid:3 cp sid:4")
+  # speed "[1 2 1 1]/2"
+@
+-}
 smash n xs p = slowcat $ map (\n -> slow n p') xs
   where p' = striate n p
 
+{- | an altenative form to `smash` is `smash'` which will use `chop` instead of `striate`.
+-}
 smash' n xs p = slowcat $ map (\n -> slow n p') xs
   where p' = chop n p
 
