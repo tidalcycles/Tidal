3fe93ef4940ab0fb6aa7546a555b85b37876cba7
alex <alex@slab.ogr>
2016-05-24 20:30:54 +0100

    fix jux / juxBy regression.. not sure what went on here.

--- Sound/Tidal/Strategies.hs
+++ Sound/Tidal/Strategies.hs
@@ -89,7 +89,7 @@ d1 $ juxBy 0.5 (density 2) $ sound "bd sn:1"
 In the above, the two versions of the pattern would be panned at 0.25
 and 0.75, rather than 0 and 1.
 -}
-juxBy n f p = stack [p |+| pan (pure $ mod' (0-(n/2)) 1), f $ p # pan (pure $ mod' (n/2) 1)]
+juxBy n f p = stack [p |+| pan (pure $ 0.5 - (n/2)), f $ p |+| pan (pure $ 0.5 + (n/2))]
 
 {- | Smash is a combination of `spread` and `striate` - it cuts the samples
 into the given number of bits, and then cuts between playing the loop
