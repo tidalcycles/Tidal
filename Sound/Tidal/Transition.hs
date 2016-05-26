9c80d8293a27059a0b8016b731f801623d751e90
Lennart Melzer <lennart@melzer.it>
2016-05-26 12:45:01 +0200

    [add] docs for `wash`

--- Sound/Tidal/Transition.hs
+++ Sound/Tidal/Transition.hs
@@ -42,6 +42,17 @@ superwash fout fin delay dur now (p:p':_) =
  where
    between lo hi x = (x >= lo) && (x < hi)
 
+{-|
+Wash away the current pattern by applying a function to it over time, then switching over to the next.
+
+@
+d1 $ sound "feel ! feel:1 feel:2"
+
+t1 (wash (chop 8) 4) $ sound "feel*4 [feel:2 sn:2]"
+@
+
+Note that `chop 8` is applied to `sound "feel ! feel:1 feel:2"` for 4 cycles and then the whole pattern is replaced by `sound "feel*4 [feel:2 sn:2]`
+-}
 wash :: (Pattern a -> Pattern a) -> Time -> Time -> [Pattern a] -> Pattern a
 wash _ _ _ [] = silence
 wash _ _ _ (p:[]) = p
