ad6dfba166c5c7d2c94016c1f588a6779cd45d76
pd3v <paulo.develop@gmail.com>
2017-01-07 12:49:30 +0000

    Tremolo's effect params definition.

--- Sound/Tidal/Params.hs
+++ Sound/Tidal/Params.hs
@@ -242,11 +242,11 @@ Using `unit "s"` means the playback speed will be adjusted so that the duration 
 (expression,expression_p)        = pF "expression" (Just 1)
 (sustainpedal,sustainpedal_p)    = pF "sustainpedal" (Just 0)
 
--- Tremolo Audio DSP effect | params are "rate" and "depth"
-
-(rate,rate_p)                    = pF "rate" (Just 1)
-(depth,depth_p)                  = pF "depth" (Just 0.5)
-
+-- Tremolo Audio DSP effect | params are "tremolorate" and "tremolodepth"
+tremolorate, tremolodepth :: Pattern Double -> ParamPattern
+tremolorate_p, tremolodepth_p :: Param
+(tremolorate,tremolorate_p)      = pF "tremolorate" (Just 1)
+(tremolodepth,tremolodepth_p)    = pF "tremolodepth" (Just 0.5)
 
 -- aliases
 att, chdecay, ctf, ctfg, delayfb, delayt, lbd, lch, lcl, lcp, lcr, lfoc, lfoi
