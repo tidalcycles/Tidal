6e6256a0419c7eb8ee95295bd6966117006a9a0b
Julian Rohrhuber <julian.rohrhuber@musikundmedien.net>
2017-03-06 19:54:33 +0100

    increase latency default to avoid late OSC  messages

--- Sound/Tidal/Dirt.hs
+++ Sound/Tidal/Dirt.hs
@@ -57,7 +57,7 @@ dirt = Shape {   params = [ s_p,
                             release_p
                           ],
                  cpsStamp = True,
-                 latency = 0.04
+                 latency = 0.3
                 }
 
 dirtSlang = OscSlang {
@@ -264,7 +264,7 @@ xfadeIn _ _ [] = silence
 xfadeIn _ _ (p:[]) = p
 xfadeIn t now (p:p':_) = overlay (p |*| gain (now ~> (slow t envEqR))) (p' |*| gain (now ~> (slow t (envEq))))
 
-{- | 
+{- |
 Crossfade between old and new pattern over the next two cycles.
 
 @
@@ -278,7 +278,7 @@ t1 xfade $ sound "can*3"
 xfade :: Time -> [ParamPattern] -> ParamPattern
 xfade = xfadeIn 2
 
-{- | Stut applies a type of delay to a pattern. It has three parameters, 
+{- | Stut applies a type of delay to a pattern. It has three parameters,
 which could be called depth, feedback and time. Depth is an integer
 and the others floating point. This adds a bit of echo:
 
@@ -286,7 +286,7 @@ and the others floating point. This adds a bit of echo:
 d1 $ stut 4 0.5 0.2 $ sound "bd sn"
 @
 
-The above results in 4 echos, each one 50% quieter than the last, 
+The above results in 4 echos, each one 50% quieter than the last,
 with 1/5th of a cycle between them. It is possible to reverse the echo:
 
 @
