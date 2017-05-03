72b64a4e8dd0379e68e0ab0d3c8efa32c1263e85
alex <alex@slab.org>
2017-05-03 20:06:11 +0100

    failing test for issue #206

--- tests/test.hs
+++ tests/test.hs
@@ -13,18 +13,22 @@ import Sound.Tidal.Context
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [sequences]
-
-sequences :: TestTree
-sequences = testGroup "Sequences" [basic1]
-
+tests = testGroup "Tests" [basic1,
+                           patternsOfPatterns
+                          ]
 
 basic1 = testGroup "fast / slow"
          [
-           testCase "silence" $ same16 "silence" (fast 1.1 silence) (silence :: Pattern Int),
-           testCase "fast" $ same16 "silence" silence (silence :: Pattern Int),
-           testCase "fast" $ same16 "bd*128" "bd*128" (rep 128 "bd")
+           testCase "silence" $ same16 (fast 1.1 silence) (silence :: Pattern Double),
+           testCase "fast" $ same16  silence (silence :: Pattern Double),
+           testCase "fast" $ same16 "bd*128" (rep 128 "bd")
          ]
+
+patternsOfPatterns =
+  testGroup "patterns of patterns"
+  [
+    testCase "decimal density" $ same16 (_discretise 0.25 saw) (discretise 0.25 saw)
+  ]
 
 rep :: Int -> String -> Pattern String
 rep n v = p $ intercalate " " $ take n $ repeat v
@@ -32,5 +36,6 @@ rep n v = p $ intercalate " " $ take n $ repeat v
 sameN :: (Eq a, Show a) => String -> Time -> Pattern a -> Pattern a -> Assertion
 sameN s n a b = assertEqual s (arc a (0,n)) (arc b (0,n))
 
-same16 s = sameN s 16
+same16 :: (Eq a, Show a) => Pattern a -> Pattern a -> Assertion
+same16 = sameN "for 16 cycles," 16
 
