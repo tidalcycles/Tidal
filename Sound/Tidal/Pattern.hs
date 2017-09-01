d616e5a4417ef7f9554e0afcf964d5f8164c3a67
pd3v <paulo.develop@gmail.com>
2017-09-01 16:18:10 +0100

    To resolve issue #220

--- Sound/Tidal/Pattern.hs
+++ Sound/Tidal/Pattern.hs
@@ -22,7 +22,7 @@ import Sound.Tidal.Utils
 import Sound.Tidal.Bjorklund
 
 import Text.Show.Functions ()
-import qualified Control.Exception as E (throw)
+import qualified Control.Exception as E (throw, ErrorCall)
 
 -- | The pattern datatype, a function from a time @Arc@ to @Event@
 -- values. For discrete patterns, this returns the events which are
