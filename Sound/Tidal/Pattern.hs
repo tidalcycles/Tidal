e7148a767e35be82275fbddbd6b94b9cbda3a982
alex <alex@slab.org>
2018-04-14 09:23:21 +0100

    remove monoid and semigroup stuff which still seems to be freezing thigns

--- Sound/Tidal/Pattern.hs
+++ Sound/Tidal/Pattern.hs
@@ -4,7 +4,7 @@
 module Sound.Tidal.Pattern where
 
 import Control.Applicative
-import Data.Monoid
+-- import Data.Monoid
 import Data.Fixed
 import Data.List
 import Data.Maybe
@@ -24,9 +24,11 @@ import Sound.Tidal.Bjorklund
 import Text.Show.Functions ()
 import qualified Control.Exception as E
 
+{-
 #ifdef TIDAL_SEMIGROUP
 import qualified Data.Semigroup as Sem
 #endif
+-}
 
 -- | The pattern datatype, a function from a time @Arc@ to @Event@
 -- values. For discrete patterns, this returns the events which are
@@ -165,6 +167,7 @@ instance Applicative Pattern where
                  (xs (s',e'))
                 )
 
+{-
 #ifdef TIDAL_SEMIGROUP
 -- | @mappend@ a.k.a. @<>@ is a synonym for @overlay@.
 instance Sem.Semigroup (Pattern a) where
@@ -180,6 +183,7 @@ instance Monoid (Pattern a) where
   mempty = silence
   mappend = overlay
 #endif
+-}
 
 instance Monad Pattern where
   return = pure
