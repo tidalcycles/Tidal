8dd2cfa71a9df5a5d7aca3cd60dcc25027d4bcc4
alex <alex@slab.org>
2018-09-29 14:30:58 +0100

    common time reference

--- src/Sound/Tidal/Tempo.hs
+++ src/Sound/Tidal/Tempo.hs
@@ -10,10 +10,10 @@ import qualified Sound.Tidal.Pattern as P
 import qualified Sound.OSC.FD as O
 import qualified Network.Socket as N
 import Control.Concurrent (forkIO, ThreadId, threadDelay)
-import Control.Monad (forM_, forever, void)
+import Control.Monad (forM_, forever, void, when)
 
 data Tempo = Tempo {at :: O.Time,
-                    cyclePos :: Double,
+                    cyclePos :: Rational,
                     cps :: O.Time,
                     paused :: Bool,
                     nudged :: Double
@@ -25,14 +25,13 @@ data State = State {ticks :: Int,
                     arc :: P.Arc
                    }
 
-defaultTempo :: IO Tempo
-defaultTempo = do now <- O.time
-                  return $ Tempo {at       = now,
-                                  cyclePos = 0,
-                                  cps      = 2,
-                                  paused   = True,
-                                  nudged   = 0
-                                 }
+defaultTempo :: O.Time -> Tempo
+defaultTempo t = Tempo {at       = t,
+                        cyclePos = 0,
+                        cps      = 2,
+                        paused   = True,
+                        nudged   = 0
+                       }
 
 
 getClockIp :: IO String
@@ -42,31 +41,24 @@ getClockPort :: IO Int
 getClockPort =
    maybe 9160 (readNote "port parse") <$> lookupEnv "TIDAL_TEMPO_PORT"
 
--- | given a Tempo and a cycle position, returns the POSIX time of
--- that cycle position
-logicalTime :: Tempo -> Double -> Double
-logicalTime t c = changeT + timeDelta
-  where cycleDelta = c - (cyclePos t)
-        timeDelta = cycleDelta / (realToFrac $ cps t)
-        changeT = O.ntpr_to_ut $ at t
-
--- | Returns the time now in terms of
+-- | Returns the given time in terms of
 -- cycles relative to metrical grid of a given Tempo
-cyclesNow :: Tempo -> IO (Double)
-cyclesNow t = do now <- O.time
-                 let delta = now - (at t)
-                     cycleDelta = (realToFrac $ cps t) * delta
-                 return $ cyclePos t + cycleDelta
+timeToCycles :: Tempo -> O.Time -> Rational
+timeToCycles tempo t = (cyclePos tempo) + (toRational cycleDelta)
+  where delta = t - (at tempo)
+        cycleDelta = (realToFrac $ cps tempo) * delta
 
+{-
 getCurrentCycle :: MVar Tempo -> IO Rational
 getCurrentCycle t = (readMVar t) >>= (cyclesNow) >>= (return . toRational)
+-}
 
 tickLength :: O.Time
-tickLength = 0.1
+tickLength = 0.125
 
-clocked :: (Tempo -> IO ()) -> IO ()
-clocked callback = do (mt, _) <- listen
-                      start <- O.time
+clocked :: (Tempo -> State -> IO ()) -> IO ()
+clocked callback = do start <- O.time
+                      (mt, _) <- listen start
                       let st = State {ticks = 0,
                                       start = start,
                                       now = start,
@@ -74,27 +66,29 @@ clocked callback = do (mt, _) <- listen
                                      }
                       loop mt st
   where loop mt st =
-          do now <- O.time
-             t <- readMVar mt
-             let next = (start st + (fromIntegral $ (ticks st)+1) * tickLength) - now
-             putStrLn "tick"
-             putStrLn $ show $ arc st
-             threadDelay (floor $ next * 1000000)
-             let s = snd $ arc st
-                 e = s + (toRational $ tickLength * (cps t))
-             loop mt $ st {ticks = (ticks st) + 1, arc = (s,e)}
+          do putStrLn $ show $ arc st
 
-listen :: IO (MVar Tempo, ThreadId)
-listen = do udp <- O.udpServer "127.0.0.1" 0
-            addr <- getClockIp
-            port <- getClockPort
-            remote_addr <- N.inet_addr addr
-            let remote_sockaddr = N.SockAddrInet (fromIntegral port) remote_addr
-            O.sendTo udp (O.Message "/hello" [O.int32 1]) remote_sockaddr
-            t <- defaultTempo
-            mt <- newMVar t
-            tempoChild <- (forkIO $ listenTempo udp mt)
-            return (mt, tempoChild)
+             tempo <- readMVar mt
+             let logicalNow = start st + (fromIntegral $ (ticks st)+1) * tickLength
+                 s = snd $ arc st
+                 e = timeToCycles tempo logicalNow
+                 st' = st {ticks = (ticks st) + 1, arc = (s,e)}
+             t <- O.time
+             when (t < logicalNow) $ threadDelay (floor $ (logicalNow - t) * 1000000)
+             callback tempo st'
+             loop mt st'
+
+listen :: O.Time -> IO (MVar Tempo, ThreadId)
+listen start = do udp <- O.udpServer "127.0.0.1" 0
+                  addr <- getClockIp
+                  port <- getClockPort
+                  remote_addr <- N.inet_addr addr
+                  let remote_sockaddr = N.SockAddrInet (fromIntegral port) remote_addr
+                      t = defaultTempo start
+                  O.sendTo udp (O.Message "/hello" [O.int32 1]) remote_sockaddr
+                  mt <- newMVar t
+                  tempoChild <- (forkIO $ listenTempo udp mt)
+                  return (mt, tempoChild)
 
 listenTempo :: O.UDP -> (MVar Tempo) -> IO ()
 listenTempo udp mt = forever $ do pkt <- O.recvPacket udp
@@ -107,43 +101,4 @@ listenTempo udp mt = forever $ do pkt <- O.recvPacket udp
              putMVar mt $ tempo {at = ts, cps = realToFrac cps'}
         act _ pkt = putStrLn $ "Unknown packet: " ++ show pkt
 
-{-
-clockedTick :: Int -> (Tempo -> Int -> IO ()) -> IO ()
-clockedTick tpb callback =
-  do (mTempo, _, mCps) <- runClient
-     t <- readMVar mTempo
-     now <- getCurrentTime
-     let delta = realToFrac $ diffUTCTime now (at t)
-         beatDelta = cps t * delta
-         nowBeat = beat t + beatDelta
-         nextTick = ceiling (nowBeat * (fromIntegral tpb))
-         -- next4 = nextBeat + (4 - (nextBeat `mod` 4))
-     loop mTempo nextTick
-  where loop mTempo tick =
-          do tempo <- readMVar mTempo
-             tick' <- doTick tempo tick
-             loop mTempo tick'
-        doTick tempo tick | paused tempo =
-          do let pause = 0.01
-             -- TODO - do this via blocking read on the mvar somehow
-             -- rather than polling
-             threadDelay $ floor (pause * 1000000)
-             -- reset tick to 0 if cps is negative
-             return $ if cps tempo < 0 then 0 else tick
-                          | otherwise =
-          do now <- getCurrentTime
-             let tps = (fromIntegral tpb) * cps tempo
-                 delta = realToFrac $ diffUTCTime now (at tempo)
-                 actualTick = ((fromIntegral tpb) * beat tempo) + (tps * delta)
-                 -- only wait by up to two ticks
-                 tickDelta = min 2 $ (fromIntegral tick) - actualTick
-                 delay = tickDelta / tps
-             -- putStrLn $ "tick delta: " ++ show tickDelta
-             --putStrLn ("Delay: " ++ show delay ++ "s Beat: " ++ show (beat tempo))
-             threadDelay $ floor (delay * 1000000)
-             callback tempo tick
-             -- putStrLn $ "hmm diff: " ++ show (abs $ (floor actualTick) - tick)
-             let newTick | (abs $ (floor actualTick) - tick) > 4 = floor actualTick
-                         | otherwise = tick + 1
-             return $ newTick
--}
+
