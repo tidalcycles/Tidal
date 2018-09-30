9a23dd5853375bfb6b659e227f440c0bc571a305
alex <alex@slab.org>
2018-09-30 18:45:48 +0100

    copy over some functions from old tidal

--- src/Sound/Tidal/Params.hs
+++ src/Sound/Tidal/Params.hs
@@ -4,6 +4,15 @@ import qualified Data.Map.Strict as Map
 
 import Sound.Tidal.Pattern
 
+ci :: String -> Pattern Int -> ControlPattern
+ci name = fmap (Map.singleton name . VI)
+
+cf :: String -> Pattern Float -> ControlPattern
+cf name = fmap (Map.singleton name . VF)
+
+cs :: String -> Pattern String -> ControlPattern
+cs name = fmap (Map.singleton name . VS)
+
 s = cs "s"
 n = cf "n"
-note = ci "note"
+note = cf "note"
