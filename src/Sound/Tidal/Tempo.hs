92421f16f332a1488c6e4ba4a956462b8382fd3d
alex <alex@slab.org>
2018-10-23 15:21:10 +0100

    starting to get a simple tempo server going

--- src/Sound/Tidal/Tempo.hs
+++ src/Sound/Tidal/Tempo.hs
@@ -8,9 +8,11 @@ import Safe (readNote)
 import Control.Concurrent.MVar
 import qualified Sound.Tidal.Pattern as P
 import qualified Sound.OSC.FD as O
+import qualified Sound.OSC.Transport.FD.UDP as O
 import qualified Network.Socket as N
 import Control.Concurrent (forkIO, ThreadId, threadDelay)
-import Control.Monad (forever, when)
+import Control.Monad (forever, when, foldM)
+import Data.List (nub,isPrefixOf)
 
 data Tempo = Tempo {atTime  :: O.Time,
                     atCycle :: Rational,
@@ -63,7 +65,7 @@ getCurrentCycle t = (readMVar t) >>= (cyclesNow) >>= (return . toRational)
 
 clocked :: (Tempo -> State -> IO ()) -> IO ()
 clocked callback = do s <- O.time
-                      (mt, _) <- listen s
+                      (mt, _) <- clientListen s
                       let st = State {ticks = 0,
                                       start = s,
                                       nowTime = s,
@@ -71,7 +73,7 @@ clocked callback = do s <- O.time
                                      }
                       loop mt st
   where loop mt st =
-          do -- putStrLn $ show $ nowArc st
+          do -- putStrLn $ show $ nowArc ts
 
              tempo <- readMVar mt
              tickLength <- getTickLength
@@ -84,28 +86,35 @@ clocked callback = do s <- O.time
              callback tempo st'
              loop mt st'
 
-listen :: O.Time -> IO (MVar Tempo, ThreadId)
-listen s = do udp <- O.udpServer "127.0.0.1" 0
-              hostname <- getClockHostname
-              port <- getClockPort
-              (remote_addr:_) <- N.getAddrInfo Nothing (Just hostname) Nothing
-              let (N.SockAddrInet _ a) = N.addrAddress remote_addr
-                  remote_sockaddr = N.SockAddrInet (fromIntegral port) (a)
-                  t = defaultTempo s
-              O.sendTo udp (O.Message "/hello" [O.int32 (1 :: Int)]) remote_sockaddr
-              mt <- newMVar t
-              tempoChild <- (forkIO $ listenTempo udp mt)
-              return (mt, tempoChild)
+clientListen :: O.Time -> IO (MVar Tempo, ThreadId)
+clientListen s =
+  do -- Listen on random port
+     udp <- O.udpServer "127.0.0.1" 0
+     hostname <- getClockHostname
+     port <- getClockPort
+     (remote_addr:_) <- N.getAddrInfo Nothing (Just hostname) Nothing
+     let (N.SockAddrInet _ a) = N.addrAddress remote_addr
+         remote_sockaddr = N.SockAddrInet (fromIntegral port) (a)
+         t = defaultTempo s
+     -- Send to clock port from same port that's listened to
+     putStrLn "sending hello."
+     O.sendTo udp (O.Message "/hello" []) remote_sockaddr
+     putStrLn "sent."
+     -- Make tempo mvar
+     mt <- newMVar t
+     -- Listen to tempo changes
+     tempoChild <- (forkIO $ listenTempo udp mt)
+     return (mt, tempoChild)
 
 listenTempo :: O.UDP -> (MVar Tempo) -> IO ()
 listenTempo udp mt = forever $ do pkt <- O.recvPacket udp
                                   act Nothing pkt
                                   return ()
   where act _ (O.Packet_Bundle (O.Bundle ts ms)) = mapM_ (act (Just ts) . O.Packet_Message) ms
-        act (Just ts) (O.Packet_Message (O.Message "/cps" [O.Float atCycle',
-                                                           O.Float cps',
-                                                           O.Int32 paused'
-                                                          ]
+        act (Just ts) (O.Packet_Message (O.Message "/cps/cycle" [O.Float atCycle',
+                                                                 O.Float cps',
+                                                                 O.Int32 paused'
+                                                                ]
                                         )
                       ) =
           do putStrLn "cps change"
@@ -118,3 +127,34 @@ listenTempo udp mt = forever $ do pkt <- O.recvPacket udp
         act _ pkt = putStrLn $ "Unknown packet: " ++ show pkt
 
 
+serverListen :: IO ()
+serverListen = do port <- getClockPort
+                  -- iNADDR_ANY deprecated - what's the right way to do this?
+                  udp <- O.udpServer "0.0.0.0" port
+                  loop udp []
+  where loop udp cs = do (pkt,c) <- O.recvFrom udp
+                         cs' <- act udp c Nothing cs pkt
+                         loop udp cs'
+        act :: O.UDP -> N.SockAddr -> Maybe O.Time -> [N.SockAddr] -> O.Packet -> IO [N.SockAddr]
+        act udp c _ cs (O.Packet_Bundle (O.Bundle ts ms)) = foldM (act udp c (Just ts)) cs $ map (O.Packet_Message) ms
+        act _ c _ cs (O.Packet_Message (O.Message "/hello" []))
+          = do putStrLn $ "hello from " ++ show c
+               return $ nub $ c:cs
+        act udp c _ cs (O.Packet_Message (O.Message path params))
+          | isPrefixOf "/transmit" path =
+              do let path' = drop 9 path
+                     msg = O.Message path' params
+                 putStrLn $ "transmit " ++ show msg
+                 mapM_ (O.sendTo udp msg) cs
+                 return cs
+        act _ c _ cs pkt = do putStrLn $ "Unknown packet: " ++ show pkt
+                              return cs
+
+{-
+
+let daveip = "192.168.0.21";
+    daveport = 8000
+    delayDave n = do dave <- openUDP daveip daveport
+                     let m = Message "/delay" [Sound.OSC.FD.float n]
+                     sendOSC dave m
+-}
