1316c92d287061bd5c711e63c4543074ffe8dac3
alex <alex@slab.org>
2018-10-30 14:44:45 +0000

    back out buggy chord parsing

--- src/Sound/Tidal/Parse.hs
+++ src/Sound/Tidal/Parse.hs
@@ -281,9 +281,12 @@ pVocable = do v <- pString
 
 pDouble :: Parser (TPat Double)
 pDouble = do f <- choice [intOrFloat, parseNote] <?> "float"
+             return $ TPat_Atom f
+             {-
              do c <- parseChord
                 return $ TPat_Stack $ map (TPat_Atom . (+f)) c
                <|> return (TPat_Atom f)
+             -}
 
 pBool :: Parser (TPat Bool)
 pBool = do oneOf "t1"
@@ -304,9 +307,12 @@ parseInt = do s <- sign
 
 pIntegral :: Parseable a => Integral a => Parser (TPat a)
 pIntegral = do i <- parseIntNote
+               return (TPat_Atom i)
+            {-
                do c <- parseChord
                   return $ TPat_Stack $ map (TPat_Atom . (+i)) c
                  <|> return (TPat_Atom i)
+             -}
 
 parseChord :: Num a => Parser [a]
 parseChord = do name <- many1 $ letter <|> digit
