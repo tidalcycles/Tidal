250e2586be65673ea0fc964017b91d8f7cc56bd3
alex <alex@slab.org>
2018-12-07 21:48:28 +0000

    have fairly high latency by default, so it is more likely to work for everyone

--- tidal.el
+++ tidal.el
@@ -93,7 +93,7 @@
       (tidal-send-string ":set prompt2 \"\"")
     (tidal-send-string ":set prompt-cont \"\""))
   (tidal-send-string "import Sound.Tidal.Context
-tidal <- startTidal (superdirtTarget {oLatency = 0.02, oAddress = \"127.0.0.1\", oPort = 57120}) (defaultConfig {cFrameTimespan = 1/20})
+tidal <- startTidal (superdirtTarget {oLatency = 0.1, oAddress = \"127.0.0.1\", oPort = 57120}) (defaultConfig {cFrameTimespan = 1/20})
 let p = streamReplace tidal
     hush = streamHush tidal
     list = streamList tidal
