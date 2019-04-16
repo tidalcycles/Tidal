37429f7d90fd531af7f7837366b43f5c6c287368
alex <alex@slab.org>
2019-04-16 23:51:01 +0100

    works!

--- src/Sound/Tidal/Carabiner.hs
+++ src/Sound/Tidal/Carabiner.hs
@@ -14,33 +14,34 @@ import qualified Sound.OSC.FD as O
 
 port = 17000
 
-start :: S.Stream -> Int -> IO Socket
-start tidal bpc = do sock <- client tidal bpc "127.0.0.1" 17000
-                     sendMsg sock "status\n"
-                     return sock
+start :: S.Stream -> Int -> Double -> IO Socket
+start tidal bpc latency = do sock <- client tidal bpc latency "127.0.0.1" 17000
+                             sendMsg sock "status\n"
+                             return sock
 
-client :: S.Stream -> Int -> String -> Int -> IO Socket
-client tidal bpc host port = withSocketsDo $
+client :: S.Stream -> Int -> Double -> String -> Int -> IO Socket
+client tidal bpc latency host port = withSocketsDo $
                        do addrInfo <- getAddrInfo Nothing (Just host) (Just $ show port)
                           let serverAddr = head addrInfo
                           sock <- socket (addrFamily serverAddr) Stream defaultProtocol
                           connect sock (addrAddress serverAddr)
-                          _ <- forkIO $ listener tidal bpc sock
+                          _ <- forkIO $ listener tidal bpc latency sock
                           -- sendMsg sock "status\n"
                           -- threadDelay 10000000
                           return sock
 
-listener :: S.Stream -> Int -> Socket -> IO ()
-listener tidal bpc sock = forever $ do rMsg <- recv sock 1024
-                                       let msg = B8.unpack rMsg
-                                           (name:_:ws) = words msg
-                                           pairs = pairs' ws
-                                           pairs' (a:b:xs) = (a,b):(pairs' xs)
-                                           pairs' _ = []
-                                       act tidal bpc name pairs
+listener :: S.Stream -> Int -> Double -> Socket -> IO ()
+listener tidal bpc latency sock =
+  forever $ do rMsg <- recv sock 1024
+               let msg = B8.unpack rMsg
+                   (name:_:ws) = words msg
+                   pairs = pairs' ws
+                   pairs' (a:b:xs) = (a,b):(pairs' xs)
+                   pairs' _ = []
+               act tidal bpc latency name pairs
 
-act :: S.Stream -> Int -> String -> [(String, String)] -> IO ()
-act tidal bpc "status" pairs
+act :: S.Stream -> Int -> Double -> String -> [(String, String)] -> IO ()
+act tidal bpc latency "status" pairs
   = do let start = (lookup ":start" pairs >>= readMaybe) :: Maybe Integer
            bpm   = (lookup ":bpm"   pairs >>= readMaybe) :: Maybe Double
            beat  = (lookup ":beat"  pairs >>= readMaybe) :: Maybe Double
@@ -49,14 +50,16 @@ act tidal bpc "status" pairs
          nowO <- O.time
          let m = (fromIntegral $ sec nowM) + ((fromIntegral $ nsec nowM)/1000000000)
              d = nowO - m
-             t = d + ((fromIntegral $ fromJust start) / 1000000)
+             start' = ((fromIntegral $ fromJust start) / 1000000)
+             startO = start' + d
+             cyc = toRational $ (fromJust beat) / (fromIntegral bpc)
          tempo <- takeMVar (S.sTempoMV tidal)
-         putMVar (S.sTempoMV tidal) $ tempo {atTime = t,
-                                             atCycle = timeToCycles tempo ((fromJust beat) * (fromIntegral bpc)),
-                                             cps = ((fromJust bpm) / 60) / (fromIntegral bpc)
-                                            }
-         putStrLn $ "bpm: " ++ (show $ fromJust bpm) ++ " cps: " ++ show (((fromJust bpm) / 60) / (fromIntegral bpc))
-act _ _ name _ = putStr $ "Unhandled thingie " ++ name
+         let tempo' = tempo {atTime = startO + latency,
+                             atCycle = 0,
+                             cps = ((fromJust bpm) / 60) / (fromIntegral bpc)
+                            }
+         putMVar (S.sTempoMV tidal) $ tempo'
+act _ _ _ name _ = putStr $ "Unhandled thingie " ++ name
 
 sendMsg :: Socket -> String -> IO ()
 sendMsg sock msg = do send sock $ B8.pack msg
