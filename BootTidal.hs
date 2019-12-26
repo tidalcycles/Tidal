b8badee2bfc2c2720a0497f63d5dfc640d9d40a0
alex <alex@slab.org>
2019-12-26 09:08:15 +0000

    set to utf8

--- BootTidal.hs
+++ BootTidal.hs
@@ -3,6 +3,9 @@
 :set prompt-cont ""
 
 import Sound.Tidal.Context
+import System.IO (hSetEncoding, stdout, utf8)
+
+hSetEncoding stdout utf8
 
 -- total latency = oLatency + cFrameTimespan
 tidal <- startTidal (superdirtTarget {oLatency = 0.1, oAddress = "127.0.0.1", oPort = 57120}) (defaultConfig {cFrameTimespan = 1/20})
