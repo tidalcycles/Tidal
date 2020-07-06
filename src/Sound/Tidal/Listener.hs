ec1678b5b29112251bb334b0914d3b45e0810e0f
alex <alex@slab.org>
2020-07-06 14:11:55 +0100

    match protocol

--- src/Sound/Tidal/Listener.hs
+++ src/Sound/Tidal/Listener.hs
@@ -12,14 +12,18 @@ https://github.com/tidalcycles/tidal-listener/wiki
 
 listenPort = 6011
 remotePort = 6012
-  
+
 listen :: IO ()
-listen = do (mIn, mOut) <- startHint
+listen = do -- start Haskell interpreter, with input and output mutable variables to
+            -- communicate with it
+            (mIn, mOut) <- startHint
+            -- listen
             udp <- udpServer "127.0.0.1" listenPort
             loop mIn mOut udp
               where
                 loop mIn mOut udp = 
-                  do m <- recvMessage udp
+                  do -- wait for, read and act on OSC message
+                     m <- recvMessage udp
                      act mIn mOut udp m
                      loop mIn mOut udp
 
@@ -29,7 +33,7 @@ startHint = do mIn <- newEmptyMVar
                return (mIn, mOut)
 
 act :: MVar String -> MVar Response -> UDP -> Maybe O.Message -> IO ()
-act mIn mOut local (Just (Message "/run" [ASCII_String a_ident, ASCII_String a_code])) =
+act mIn mOut local (Just (Message "/code" [ASCII_String a_ident, ASCII_String a_code])) =
   do (remote_addr:_) <- N.getAddrInfo Nothing (Just "127.0.0.1") Nothing
      let ident = ascii_to_string a_ident
          code = ascii_to_string a_code
@@ -41,8 +45,8 @@ act mIn mOut local (Just (Message "/run" [ASCII_String a_ident, ASCII_String a_c
      r <- takeMVar mOut
      respond r
      return ()
-       where sendOK remote ident = O.sendTo local (O.p_message "/ok" [string ident]) remote
-             sendError remote ident s = O.sendTo local (O.p_message "/error" [string ident, string s]) remote
+       where sendOK remote ident = O.sendTo local (O.p_message "/code/ok" [string ident]) remote
+             sendError remote ident s = O.sendTo local (O.p_message "/code/error" [string ident, string s]) remote
 
 act _ _ _ Nothing = do putStrLn "not a message?"
                        return ()
