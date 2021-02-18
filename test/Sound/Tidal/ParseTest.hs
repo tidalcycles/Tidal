tidalcycles/Tidal

commit 262e082cd5c33c824452d93fdcf972d4ce12ca56

Format code

src/Sound/Tidal/ParseBP.hs
@@ -333,28 +333,32 @@ parseRhythm f = runParser (pSequence f' Prelude.<* eof) (0 :: Int) ""
                        return TPat_Silence
 
 pSequence :: Parseable a => MyParser (TPat a) -> GenParser Char Int (TPat a)
-pSequence f = do spaces
-                 s <- many $ do a <- pPart f
-                                spaces
-                                do try $ symbol ".."
-                                   b <- pPart f
-                                   return $ TPat_EnumFromTo a b
-                                 <|> pElongate a
-                                 <|> pRepeat a
-                                 <|> return a
-                             <|> do symbol "."
-                                    return TPat_Foot
-                 pRand $ resolve_feet s
-      where resolve_feet ps | length ss > 1 = TPat_Seq $ map TPat_Seq ss
-                            | otherwise = TPat_Seq ps
-              where ss = splitFeet ps
-            splitFeet :: [TPat t] -> [[TPat t]]
-            splitFeet [] = []
-            splitFeet pats = foot : splitFeet pats'
-              where (foot, pats') = takeFoot pats
-                    takeFoot [] = ([], [])
-                    takeFoot (TPat_Foot:pats'') = ([], pats'')
-                    takeFoot (pat:pats'') = (\(a,b) -> (pat:a,b)) $ takeFoot pats''
+pSequence f = do
+  spaces
+  s <- many $ do
+    a <- pPart f
+    spaces
+    do
+      symbol ".."
+      b <- pPart f
+      return $ TPat_EnumFromTo a b
+      <|> pElongate a
+      <|> pRepeat a
+      <|> return a
+    <|> do
+      symbol "."
+      return TPat_Foot
+  pRand $ resolve_feet s
+  where resolve_feet ps | length ss > 1 = TPat_Seq $ map TPat_Seq ss
+                        | otherwise = TPat_Seq ps
+          where ss = splitFeet ps
+        splitFeet :: [TPat t] -> [[TPat t]]
+        splitFeet [] = []
+        splitFeet pats = foot : splitFeet pats'
+          where (foot, pats') = takeFoot pats
+                takeFoot [] = ([], [])
+                takeFoot (TPat_Foot:pats'') = ([], pats'')
+                takeFoot (pat:pats'') = (\(a,b) -> (pat:a,b)) $ takeFoot pats''
 
 pRepeat :: TPat a -> MyParser (TPat a)
 pRepeat a = do es <- many1 $ do char '!'

commit 7d1c49884117fe170646c4017add8ff882ac2a37

Fix parsing of pattern groupping shorthand
Regression was introduced in 262e082cd5c33c824452d93fdcf972d4ce12ca56
during code reformating as a part of PR #776.

This caused basic patterns like `a . b` fail rather confusingly:

    Error in pattern: Syntax error in sequence:
      "a . b"
         ^
    unexpected " "
    expecting ".."

This commit brings back the forgotten `try` for `symbol ".."` and adds
parsing tests for a pattern group and its shorthand form.

src/Sound/Tidal/ParseBP.hs
@@ -339,7 +339,7 @@ pSequence f = do
     a <- pPart f
     spaces
     do
-      symbol ".."
+      try $ symbol ".."
       b <- pPart f
       return $ TPat_EnumFromTo a b
       <|> pElongate a

test/Sound/Tidal/ParseTest.hs
@@ -23,6 +23,14 @@ run =
         compareP (Arc 0 2)
           ("0 1 2 3 4 5 6 7 8 0 10 20 30 40 50" :: Pattern Int)
           (fastCat $ map (pure . read) $ words "0 1 2 3 4 5 6 7 8 0 10 20 30 40 50")
+      it "can parse pattern groups" $ do
+        compareP (Arc 0 1)
+          ("[bd sd] hh" :: Pattern String)
+          (fastCat ["bd sd", "hh"])
+      it "can parse pattern groups shorthand " $ do
+        compareP (Arc 0 1)
+          ("bd sd . hh hh hh" :: Pattern String)
+          ("[bd sd] [hh hh hh]")
       it "can alternate with <>" $ do
         compareP (Arc 0 2)
           ("a <b c>" :: Pattern String)