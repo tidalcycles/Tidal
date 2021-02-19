8b74fcea06e9d10e9b40f27f283a7594eeaa5090
alex <alex@slab.org>
2021-02-19 14:16:55 +0000

    separate module for event

---
+++ src/Sound/Tidal/Event.hs
@@ -0,0 +1,102 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+module Sound.Tidal.Event where
+
+import           Control.DeepSeq (NFData)
+import           GHC.Generics
+import Data.Maybe (isJust, fromJust, catMaybes, mapMaybe)
+import Data.List (delete, findIndex, sort)
+
+import           Sound.Tidal.Time
+
+-- | Some context for an event, currently just position within sourcecode
+data Context = Context {contextPosition :: [((Int, Int), (Int, Int))]}
+  deriving (Eq, Ord, Generic)
+instance NFData Context
+
+-- | An event is a value that's active during a timespan. If a whole
+-- is present, the part should be equal to or fit inside it.
+data EventF a b = Event
+  { context :: Context
+  , whole :: Maybe a
+  , part :: a
+  , value :: b
+  } deriving (Eq, Ord, Functor, Generic)
+instance (NFData a, NFData b) => NFData (EventF a b)
+
+type Event a = EventF (ArcF Time) a
+
+-- * Event utilities
+
+isAnalog :: Event a -> Bool
+isAnalog (Event {whole = Nothing}) = True
+isAnalog _ = False
+
+isDigital :: Event a -> Bool
+isDigital = not . isAnalog
+
+-- | `True` if an `Event`'s starts is within given `Arc`
+onsetIn :: Arc -> Event a -> Bool
+onsetIn a e = isIn a (wholeStart e)
+
+-- | Compares two lists of events, attempting to combine fragmented events in the process
+-- for a 'truer' compare
+compareDefrag :: (Ord a) => [Event a] -> [Event a] -> Bool
+compareDefrag as bs = sort (defragParts as) == sort (defragParts bs)
+
+-- | Returns a list of events, with any adjacent parts of the same whole combined
+defragParts :: Eq a => [Event a] -> [Event a]
+defragParts [] = []
+defragParts [e] = [e]
+defragParts (e:es) | isJust i = defraged : defragParts (delete e' es)
+                   | otherwise = e : defragParts es
+  where i = findIndex (isAdjacent e) es
+        e' = es !! fromJust i
+        defraged = Event (context e) (whole e) u (value e)
+        u = hull (part e) (part e')
+
+-- | Returns 'True' if the two given events are adjacent parts of the same whole
+isAdjacent :: Eq a => Event a -> Event a -> Bool
+isAdjacent e e' = (whole e == whole e')
+                  && (value e == value e')
+                  && ((stop (part e) == start (part e'))
+                      ||
+                      (stop (part e') == start (part e))
+                     )
+
+wholeOrPart :: Event a -> Arc
+wholeOrPart (Event {whole = Just a}) = a
+wholeOrPart e = part e
+
+-- | Get the onset of an event's 'whole'
+wholeStart :: Event a -> Time
+wholeStart = start . wholeOrPart
+
+-- | Get the offset of an event's 'whole'
+wholeStop :: Event a -> Time
+wholeStop = stop . wholeOrPart
+
+-- | Get the onset of an event's 'whole'
+eventPartStart :: Event a -> Time
+eventPartStart = start . part
+
+-- | Get the offset of an event's 'part'
+eventPartStop :: Event a -> Time
+eventPartStop = stop . part
+
+-- | Get the timespan of an event's 'part'
+eventPart :: Event a -> Arc
+eventPart = part
+
+eventValue :: Event a -> a
+eventValue = value
+
+eventHasOnset :: Event a -> Bool
+eventHasOnset e | isAnalog e = False
+                | otherwise = start (fromJust $ whole e) == start (part e)
+
+-- TODO - Is this used anywhere? Just tests, it seems
+-- TODO - support 'context' field
+toEvent :: (((Time, Time), (Time, Time)), a) -> Event a
+toEvent (((ws, we), (ps, pe)), v) = Event (Context []) (Just $ Arc ws we) (Arc ps pe) v
