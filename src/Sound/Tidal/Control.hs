c457448ca2fd7c3d8c20695f060fb5ccbdd90b85
onthepeakofnormal <gius_martin@yahoo.de>
2021-05-28 15:26:13 +0200

    fixes #830: minor change to _splice so that it respects if the speed parameter was already declared

--- src/Sound/Tidal/Control.hs
+++ src/Sound/Tidal/Control.hs
@@ -285,7 +285,9 @@ randslice = tParam $ \n p -> innerJoin $ (\i -> _slice n i p) <$> _irand n
 
 _splice :: Int -> Pattern Int -> ControlPattern -> Pattern (Map.Map String Value)
 _splice bits ipat pat = withEvent f (slice (pure bits) ipat pat) # P.unit (pure "c")
-  where f ev = ev {value = Map.insert "speed" (VF d) (value ev)}
+  where f ev = case Map.lookup "speed" (value ev) of
+                        Nothing -> ev {value = Map.insert "speed" (VF d) (value ev)}
+                        (Just (VF s)) -> ev {value = Map.insert "speed" (VF $ d*s) (value ev)}  -- if there is a speed parameter already present
           where d = sz / fromRational (wholeStop ev - wholeStart ev)
                 sz = 1/fromIntegral bits
 
