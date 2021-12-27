d67cfb80a350514570b8983d421dde16b4175fce
onthepeakofnormal <gius_martin@yahoo.de>
2021-12-27 11:06:33 +0100

    deriving typeclass memberships for ID

--- src/Sound/Tidal/ID.hs
+++ src/Sound/Tidal/ID.hs
@@ -23,7 +23,8 @@ import GHC.Exts ( IsString(..) )
 -- | Wrapper for literals that can be coerced to a string and used as an identifier.
 -- | Similar to Show typeclass, but constrained to strings and integers and designed
 -- | so that similar cases (such as 1 and "1") convert to the same value.
-newtype ID = ID { fromID :: String }
+newtype ID = ID { fromID :: String } deriving (Eq, Show, Ord, Read)
+
 
 noOv :: String -> a
 noOv meth = error $ meth ++ ": not supported for ids"
