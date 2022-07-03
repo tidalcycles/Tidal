f3a9e7a3c08088f9211350c50dadfa2ddf8af631
Pierre Krafft <kpierre@outlook.com>
2022-07-03 20:18:30 +0200

    Use ableton link for scheduling
    https://www.ableton.com/en/link/
    Ableton Link keeps devices in time over a local network.
    
    This change replaces other methods of synchronizing Tidal sessions with
    each other. Tidal sessions (and other Link enabled units) will now
    automatically connect and synchronize their timelines.
    
    To stop Tidal from connecting to other peers,
    configure cEnableLink = False.
    
    Ableton Link has a 20 bpm lower limit:
    https://github.com/Ableton/link/issues/59
    
    COMPATIBILITY NOTE:
    Ableton Link is a C++ library. The support for using C++ libraries in
    Haskell has improved over time, but toolchains that are still in use by
    many might not be compatible with this change.
    
    To ensure compatibility, please update your Haskell toolchain.
    Use Cabal version 3.4.0.0 or later.
    On Windows, use GHC 9.4.1.
      (see https://gitlab.haskell.org/ghc/ghc/-/issues/20918)

--- BootTidal.hs
+++ BootTidal.hs
@@ -6,7 +6,6 @@ import Sound.Tidal.Context
 import System.IO (hSetEncoding, stdout, utf8)
 hSetEncoding stdout utf8
 
--- total latency = oLatency + cFrameTimespan
 tidal <- startTidal (superdirtTarget {oLatency = 0.1, oAddress = "127.0.0.1", oPort = 57120}) (defaultConfig {cVerbose = True, cFrameTimespan = 1/20})
 
 :{
---
+++ tidal-link/src/hs/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Sound.Tidal.Link (hello)
+
+main :: IO ()
+main = hello
