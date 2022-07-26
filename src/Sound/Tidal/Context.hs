a28d1c28c083db4ab72bf0c9efba8ffdbd13c527
Alex McLean <alex@slab.org>
2022-07-26 17:08:34 +0100

    merge Common.hs into Context.hs

--- src/Sound/Tidal/Context.hs
+++ src/Sound/Tidal/Context.hs
@@ -22,18 +22,56 @@ import Prelude hiding ((<*), (*>))
 
 import Data.Ratio as C
 
-import Sound.Tidal.Common as C
 import Sound.Tidal.Config as C
 import Sound.Tidal.Control as C
-import Sound.Tidal.Core as C hiding (rev, cat)
+import Sound.Tidal.Core as C hiding (rev, cat, ply)
 import Sound.Tidal.Params as C
 import Sound.Tidal.ParseBP as C
 import Sound.Tidal.Pattern as C
 import Sound.Tidal.Scales as C
-import Sound.Tidal.Sequence as C hiding (rev, cat)
+import Sound.Tidal.Sequence as C hiding (rev, cat, ply)
 import Sound.Tidal.Show as C
 import Sound.Tidal.Simple as C
 import Sound.Tidal.Stream as C
 import Sound.Tidal.Transition as C
 import Sound.Tidal.UI as C
 import Sound.Tidal.Version as C
+
+import Sound.Tidal.Pattern as Pat
+import Sound.Tidal.Core as Pat
+import Sound.Tidal.UI as Pat
+
+import Sound.Tidal.Sequence as Seq
+
+class Transformable f where
+  rev :: f a -> f a
+  cat :: [f a] -> f a
+
+instance Transformable Pattern where
+  rev = Pat.rev
+  cat = Pat.cat
+
+instance Transformable Sequence where
+  rev = Seq.rev
+  cat = Seq.cat
+
+seqPat :: Seq.Sequence a -> Pat.Pattern a
+seqPat (Seq.Atom _ a) = pure a
+seqPat (Seq.Silence _) = Pat.silence
+seqPat (Seq.Sequence bs) = Pat.timecat $ map (\b -> (seqSpan b, seqPat b)) bs
+seqPat (Seq.Stack Expand bs) = Pat.stack $ map seqPat bs
+seqPat b@(Seq.Stack JustifyLeft bs) =
+  Pat.stack $ map (\b' -> _fastGap (seqSpan b / seqSpan b') $ seqPat b') bs
+seqPat b@(Seq.Stack JustifyRight bs) =
+  Pat.stack $
+    map (\b' -> rotR (1- (1/(seqSpan b / seqSpan b'))) $ _fastGap (seqSpan b / seqSpan b') $ seqPat b') bs
+seqPat b@(Seq.Stack Centre bs) = Pat.stack $
+    map (\b' -> rotR (1.5/(seqSpan b / seqSpan b')) $ _fastGap (seqSpan b / seqSpan b') $ seqPat b') bs
+
+{-
+data Strategy = JustifyBoth
+              | Expand
+              | TruncateMax
+              | TruncateMin
+              | RepeatLCM
+-}
