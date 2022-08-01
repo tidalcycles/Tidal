b76c217fe60e2e1e4ec1c4c2d3747be8aa3d7d19
aravind-mohandas <aravindmohandas.2k@gmail.com>
2022-08-01 18:19:38 +0530

    Added squeeze strategy and fast method

--- src/Sound/Tidal/Sequence.hs
+++ src/Sound/Tidal/Sequence.hs
@@ -34,19 +34,22 @@ data Strategy = JustifyLeft
               | TruncateMin
               | RepeatLCM
               | Centre
+              | Squeeze
               deriving Show
 
 data Sequence a = Atom Rational a
                 | Gap Rational
                 | Sequence [Sequence a]
-                | Stack Strategy [Sequence a]
+                | Stack [Sequence a]
               deriving Show
+
+
 
 instance Functor Sequence where
   fmap f (Atom x y) = Atom x (f y)
   fmap f (Gap x) = Gap x
   fmap f (Sequence x) = Sequence $ map (fmap f) x
-  fmap f (Stack x y) = Stack x (map (fmap f) y)
+  fmap f (Stack y) = Stack (map (fmap f) y)
 
 instance Applicative Sequence where
   pure x = Atom 1 x
@@ -63,9 +66,16 @@ instance Applicative Sequence where
         c = unwrap $ reAlign p q
         d = Sequence$unwrap  $ funcMap p c
     in d
-  Stack x f <*> something =
+  Stack f <*> something =
     let d = map (<*> something) f
-    in Stack x d
+    in Stack d
+
+instance Monad Sequence where
+  return x = Atom 1 x
+  Atom x y >>= f = f y
+  Gap x >>= f = Gap x
+  Sequence x >>= f = Sequence $ map (>>= f) x
+  Stack y >>= f = Stack (map (>>= f) y)
 
 -- | Takes sequence of functions and a sequence which have been aligned and applies the functions at the corresponding time points
 funcMap :: [Sequence (a->b)] -> [Sequence a] -> [Sequence b]
@@ -130,16 +140,9 @@ unwrap (Sequence x :xs) = unwrap x ++ unwrap xs
 unwrap (Atom x s:xs) = Atom x s : unwrap xs
 unwrap (Gap x:xs) = if x ==0 then unwrap xs else Gap x: unwrap xs
 
-instance Monad Sequence where
-  return x = Atom 1 x
-  Atom x y >>= f = f y
-  Gap x >>= f = Gap x
-  Sequence x >>= f = Sequence $ map (>>= f) x
-  Stack x y >>= f = Stack x (map (>>= f) y)
-
 rev :: Sequence a -> Sequence a
 rev (Sequence bs) = Sequence $ reverse $ map rev bs
-rev (Stack strategy bs) = Stack strategy $ map rev bs
+rev (Stack bs) = Stack $ map rev bs
 rev b = b
 
 cat :: [Sequence a] -> Sequence a
@@ -154,11 +157,8 @@ seqSpan :: Sequence a -> Rational
 seqSpan (Atom s _) = s
 seqSpan (Gap s) = s
 seqSpan (Sequence bs) = sum $ map seqSpan bs
-seqSpan (Stack _ []) = 0
-seqSpan (Stack RepeatLCM [b]) = seqSpan b
-seqSpan (Stack RepeatLCM (b:bs)) = foldr (lcmRational . seqSpan) (seqSpan b) (b:bs)
-seqSpan (Stack TruncateMin (b:bs)) = minimum $ map seqSpan bs
-seqSpan (Stack _ bs) = maximum $ map seqSpan bs
+seqSpan (Stack []) = 0
+seqSpan (Stack x) = seqSpan $ head x
 
 lcmRational :: Rational->Rational-> Rational
 lcmRational a b = lcm (f a) (f b) % d
@@ -170,44 +170,49 @@ stratApply::Strategy -> [Sequence a] ->Sequence a
 stratApply JustifyLeft bs =
   let a = maximum $ map seqSpan bs
       b = map (\x -> Sequence (x: [Gap (a - seqSpan x)])) bs
-  in Stack JustifyLeft b
+  in Stack b
 
 stratApply JustifyRight bs =
   let a = maximum $ map seqSpan bs
       b = map (\x -> Sequence (Gap (a - seqSpan x) : [x])) bs
-  in Stack JustifyRight b
+  in Stack b
 
 stratApply Centre bs =
   let a = maximum $ map seqSpan bs
       b = map( \x -> Sequence ([Gap ((a - seqSpan x)/2)] ++ [x] ++ [Gap ((a - seqSpan x)/2)])) bs
-  in Stack Centre b
+  in Stack b
 
 stratApply RepeatLCM bs@(x:xs) =
   let a = foldr (lcmRational . seqSpan) (seqSpan x) xs
       b = map (\x ->  Sequence $ unwrap $  replicate (fromIntegral $ numerator $ a/seqSpan x) x) bs
-  in Stack RepeatLCM b
+  in Stack b
 
 stratApply Expand bs =
   let a = maximum $ map seqSpan bs
       b = map (\x -> expand x $ a/seqSpan x) bs
-  in Stack Expand b
+  in Stack b
 
+stratApply Squeeze bs = 
+  let a = minimum $ map seqSpan bs
+      b = map (\x -> expand x $ a/seqSpan x) bs
+  in Stack b
 
 stratApply JustifyBoth bs =
   let a = maximum $ map seqSpan bs
       b = map (`expand'` a) bs
-  in Stack JustifyBoth b
+  in Stack b
 
 stratApply TruncateMin bs =
   let a = minimum $ map seqSpan bs
-      b = map (\x -> cutShort x a) bs
-  in Stack TruncateMin b
+      b = map (`cutShort` a) bs
+  in Stack b
 
 stratApply TruncateMax bs =
   let a = maximum $ map seqSpan bs
       b = map (\x -> Sequence $ unwrap $ replicate (floor $ a/seqSpan x) x ++ let Sequence p = cutShort x (realToFrac (a - floor (a/seqSpan x)%1 * seqSpan x)) in p) bs
-  in Stack TruncateMax b
+  in Stack b
 
+-- Return a segment of a sequence
 cutShort::Sequence a->Rational->Sequence a
 cutShort (Atom x s) r = Atom r s
 cutShort (Gap x) r = Gap r
@@ -220,7 +225,9 @@ cutShort (Sequence x) r =
         let (m,n) =  getPartition (x!!l) (r - tTill)
         in (m, n :drop (l+1) x)
   in if tTill == r then Sequence pTill else Sequence $ pTill ++ [fst (getPartition (x!!l) (r - tTill))]
+cutShort (Stack x) r = Stack $ map (`cutShort` r) x
 
+-- | Expand a sequence to a particular length, while not modifying the length of that sequence
 expand'::Sequence a -> Rational -> Sequence a
 expand' (Atom x s) r = expand (Atom x s) $ r/seqSpan (Atom x s)
 expand' (Gap x) r = expand (Gap x) $ r/seqSpan (Gap x)
@@ -228,29 +235,50 @@ expand' (Sequence [x]) r = Sequence [expand x (r/seqSpan x)]
 expand' (Sequence x) r =
   let Sequence y = (expand (Sequence $ init x) $ (r- seqSpan (last x))/ seqSpan (Sequence $ init x))
   in Sequence (y++ [last x])
+expand' (Stack x) r = Stack $ map (`expand'` r) x
 
+-- | Expand a sequence to a particular length
 expand::Sequence a-> Rational -> Sequence a
 expand (Atom x s) r = Atom (x*r) s
 expand (Gap x) r = Gap (x*r)
 expand (Sequence x) r = Sequence $ map (`expand` r) x
-expand (Stack s x) r = Stack s $ map (`expand` r) x
+expand (Stack x) r = Stack $ map (`expand` r) x
 
+-- | Reduce a Sequence of sequences by using reduce
 reduceSeq ::Eq a=> Sequence a -> Sequence a
 reduceSeq (Sequence x) = Sequence $ reduce x
 
+-- | Reduce a list of sequences by removing redundancies
 reduce::Eq a=>[Sequence a] -> [Sequence a]
 reduce ((Atom x1 s1):(Atom x2 s2):xs) = if s1==s2 then reduce $ Atom (x1+x2) s1:xs else Atom x1 s1:reduce ( Atom x2 s2:  xs)
 reduce (Gap x1:Gap x2:xs) =reduce $ Gap (x1+x2):xs
 reduce (Sequence x:xs) = Sequence (reduce x):reduce xs
-reduce (Stack s x:xs) = Stack s (reduce x):reduce xs 
+reduce (Stack x:xs) = Stack (reduce x):reduce xs
 reduce (x:xs) = x:reduce xs
 reduce [] = []
 
--- TODO
+fast::Rational->Sequence a-> Sequence a
+fast n (Atom x s) = Atom (x/n) s
+fast n (Gap x) = Gap (x/n)
+fast n (Sequence s) = Sequence $ map (\y -> fast n y) s
+fast n (Stack x) = Stack $ map(\y -> fast n y) x
 
--- Monad instance of sequences
 
--- Method to reduce sequences to their simplest form 
+{-
+   EXISTING STUFF IN TIDAL
+   Method for showing
+   [] -> Play in double the speed
+   *n -> plays it n times within the step
+   /m -> Plays only once in m steps
+   !n -> Repeat it n times
+   [,] -> Overlays(Polyrhythm)
+   {,} -> Polymeter
+   < > Plays one among for each iteration
+   () -> Euclidean structures
+   -}
 
-
-
+{-
+  New stuff that I would like to add
+  * Method which takes an integer and splits sequence recursively at those intervals (fitToTime)
+  * 
+-}
