59459ba5a93dee426ee637e804f618c59f47b12f
aravind-mohandas <aravindmohandas.2k@gmail.com>
2022-08-01 15:12:22 +0530

    Added methods to reduce sequences

--- src/Sound/Tidal/Sequence.hs
+++ src/Sound/Tidal/Sequence.hs
@@ -61,13 +61,13 @@ instance Applicative Sequence where
   Sequence f <*> something =
     let (p,q) = forTwo (Sequence f) something
         c = unwrap $ reAlign p q
-        d = Sequence$ unwrap  $ funcMap p c
+        d = Sequence$unwrap  $ funcMap p c
     in d
   Stack x f <*> something =
     let d = map (<*> something) f
     in Stack x d
 
-
+-- | Takes sequence of functions and a sequence which have been aligned and applies the functions at the corresponding time points
 funcMap :: [Sequence (a->b)] -> [Sequence a] -> [Sequence b]
 funcMap [] _ = []
 funcMap ((Atom x f):xs) y =
@@ -77,6 +77,8 @@ funcMap ((Atom x f):xs) y =
       q = map (\x -> fmap f x) pTill
   in q ++ funcMap xs (drop (length pTill) y)
 
+-- | Takes two sequences, which have been obtained using stratApply, or forTwo, and then splits the second sequence with respect
+-- to the break points of the first
 reAlign:: [Sequence a] -> [Sequence b] -> [Sequence b]
 reAlign [] _ = []
 reAlign a@((Gap x):xs) y =
@@ -100,6 +102,7 @@ reAlign a@((Atom x s):xs) y =
         in (m, n :drop (l+1) y)
   in (pTill ++ [a]) ++ reAlign xs b
 
+-- | Function to partition an event in a sequence
 getPartition::Sequence a-> Rational -> (Sequence a, Sequence a)
 getPartition (Atom x s) t = (Atom t s, Atom (x-t) s)
 getPartition (Gap x) t = (Gap t, Gap (x - t))
@@ -113,11 +116,13 @@ getPartition (Sequence y) t =
         in (m, n :drop (l+1) y)
   in (Sequence $ pTill ++ [a], Sequence b)
 
+-- | Given two sequences of different types, this function uses the LCM method to align those two sequences
 forTwo :: Sequence a1 -> Sequence a2 -> ([Sequence a1], [Sequence a2])
 forTwo a b =
   let p = lcmRational (seqSpan a) (seqSpan b)
   in (unwrap $ replicate (fromIntegral  $ numerator $ p/seqSpan a) a, unwrap $ replicate (fromIntegral $ numerator $ p/seqSpan b) b)
 
+-- | Unwrapping a sequence referes to removing the redundancies that are present in the code
 unwrap:: [Sequence a] -> [Sequence a]
 unwrap [] = []
 unwrap [Sequence x] = unwrap x
@@ -160,6 +165,7 @@ lcmRational a b = lcm (f a) (f b) % d
   where d = lcm (denominator a) (denominator b)
         f x = numerator x * (d `div` denominator x)
 
+-- | stratApply takes a list of sequences, a strategy, and then aligns all those sequences according to these strategies
 stratApply::Strategy -> [Sequence a] ->Sequence a
 stratApply JustifyLeft bs =
   let a = maximum $ map seqSpan bs
@@ -181,27 +187,70 @@ stratApply RepeatLCM bs@(x:xs) =
       b = map (\x ->  Sequence $ unwrap $  replicate (fromIntegral $ numerator $ a/seqSpan x) x) bs
   in Stack RepeatLCM b
 
-
---JusttifyBoth
-
---Expand
-
---TruncateMax
-
---TruncateMin
-
--- Functor for sequences
--- Applicative
--- Monad 
+stratApply Expand bs =
+  let a = maximum $ map seqSpan bs
+      b = map (\x -> expand x $ a/seqSpan x) bs
+  in Stack Expand b
 
 
--- When applying a strategy on sequences, return in standardized form?
--- Maybe return in form of the first sequence or something?
--- So when stacking it would be standardized to the first sequence in list w.r.t the duration 
--- Like for each event in the first sequence, we will have an event or a sequence of events in the second sequence.
--- This will make mapping these sequences really easy
+stratApply JustifyBoth bs =
+  let a = maximum $ map seqSpan bs
+      b = map (`expand'` a) bs
+  in Stack JustifyBoth b
 
--- Method to unwrap sequences as well -- Done
+stratApply TruncateMin bs =
+  let a = minimum $ map seqSpan bs
+      b = map (\x -> cutShort x a) bs
+  in Stack TruncateMin b
+
+stratApply TruncateMax bs =
+  let a = maximum $ map seqSpan bs
+      b = map (\x -> Sequence $ unwrap $ replicate (floor $ a/seqSpan x) x ++ let Sequence p = cutShort x (realToFrac (a - floor (a/seqSpan x)%1 * seqSpan x)) in p) bs
+  in Stack TruncateMax b
+
+cutShort::Sequence a->Rational->Sequence a
+cutShort (Atom x s) r = Atom r s
+cutShort (Gap x) r = Gap r
+cutShort (Sequence x) r =
+  let p = takeWhile (\q -> sum (map seqSpan q) <=r ) $ inits x
+      pTill = last p
+      l = length pTill
+      tTill = sum (map seqSpan pTill)
+      (a,b) = if tTill == r then (Gap 0, drop l x)  else
+        let (m,n) =  getPartition (x!!l) (r - tTill)
+        in (m, n :drop (l+1) x)
+  in if tTill == r then Sequence pTill else Sequence $ pTill ++ [fst (getPartition (x!!l) (r - tTill))]
+
+expand'::Sequence a -> Rational -> Sequence a
+expand' (Atom x s) r = expand (Atom x s) $ r/seqSpan (Atom x s)
+expand' (Gap x) r = expand (Gap x) $ r/seqSpan (Gap x)
+expand' (Sequence [x]) r = Sequence [expand x (r/seqSpan x)]
+expand' (Sequence x) r =
+  let Sequence y = (expand (Sequence $ init x) $ (r- seqSpan (last x))/ seqSpan (Sequence $ init x))
+  in Sequence (y++ [last x])
+
+expand::Sequence a-> Rational -> Sequence a
+expand (Atom x s) r = Atom (x*r) s
+expand (Gap x) r = Gap (x*r)
+expand (Sequence x) r = Sequence $ map (`expand` r) x
+expand (Stack s x) r = Stack s $ map (`expand` r) x
+
+reduceSeq ::Eq a=> Sequence a -> Sequence a
+reduceSeq (Sequence x) = Sequence $ reduce x
+
+reduce::Eq a=>[Sequence a] -> [Sequence a]
+reduce ((Atom x1 s1):(Atom x2 s2):xs) = if s1==s2 then reduce $ Atom (x1+x2) s1:xs else Atom x1 s1:reduce ( Atom x2 s2:  xs)
+reduce (Gap x1:Gap x2:xs) =reduce $ Gap (x1+x2):xs
+reduce (Sequence x:xs) = Sequence (reduce x):reduce xs
+reduce (Stack s x:xs) = Stack s (reduce x):reduce xs 
+reduce (x:xs) = x:reduce xs
+reduce [] = []
+
+-- TODO
+
+-- Monad instance of sequences
+
+-- Method to reduce sequences to their simplest form 
 
 
 
