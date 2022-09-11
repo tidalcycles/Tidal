27c03fb13743c70db840ba83c0cf0fec611a92f6
Frederik N. A. Bous <f.n.a.bous@me.com>
2022-09-11 10:22:46 +0200

    Adjusted numbers for new rng in test

--- test/Sound/Tidal/UITest.hs
+++ test/Sound/Tidal/UITest.hs
@@ -121,23 +121,24 @@ run =
     describe "rand" $ do
       it "generates a (pseudo-)random number between zero & one" $ do
         it "at the start of a cycle" $
-          (queryArc rand (Arc 0 0)) `shouldBe` [Event (Context []) Nothing (Arc 0 0) (0 :: Float)]
+          (queryArc rand (Arc 0 0)) `shouldBe`
+            [Event (Context []) Nothing (Arc 0 0) (0.138229 :: Float)]
         it "at 1/4 of a cycle" $
           (queryArc rand (Arc 0.25 0.25)) `shouldBe`
-            [Event (Context []) Nothing (Arc 0.25 0.25) (0.6295689214020967:: Float)]
+            [Event (Context []) Nothing (Arc 0.25 0.25) (0.14715718:: Float)]
         it "at 3/4 of a cycle" $
           (queryArc rand (Arc 0.75 0.75)) `shouldBe`
-          [Event (Context []) Nothing (Arc 0.75 0.75) (0.20052618719637394 :: Float)]
+          [Event (Context []) Nothing (Arc 0.75 0.75) (0.78684306 :: Float)]
 
     describe "irand" $ do
       it "generates a (pseudo-random) integer between zero & i" $ do
         it "at the start of a cycle" $
-          (queryArc (irand 10) (Arc 0 0)) `shouldBe` [Event (Context []) Nothing (Arc 0 0) (0 :: Int)]
+          (queryArc (irand 10) (Arc 0 0)) `shouldBe` [Event (Context []) Nothing (Arc 0 0) (1 :: Int)]
         it "at 1/4 of a cycle" $
-          (queryArc (irand 10) (Arc 0.25 0.25)) `shouldBe` [Event (Context []) Nothing (Arc 0.25 0.25) (6 :: Int)]
+          (queryArc (irand 10) (Arc 0.25 0.25)) `shouldBe` [Event (Context []) Nothing (Arc 0.25 0.25) (1 :: Int)]
         it "is patternable" $
           (queryArc (irand "10 2") (Arc 0 1)) `shouldBe` [
-            Event (Context [((1,1),(3,1))]) Nothing (Arc 0 0.5) (6 :: Int), Event (Context [((4,1),(5,1))]) Nothing (Arc 0.5 1) (0 :: Int)
+            Event (Context [((1,1),(3,1))]) Nothing (Arc 0 0.5) (1 :: Int), Event (Context [((4,1),(5,1))]) Nothing (Arc 0.5 1) (1 :: Int)
           ]
 
     describe "range" $ do
