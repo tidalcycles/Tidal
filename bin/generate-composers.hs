d6375a2d970129ae1861b0259efbd59874b1b2f6
Alex McLean <alex@slab.org>
2022-09-16 13:44:57 +0100

    try generating Sound.Tidal.Compose

---
+++ bin/generate-composers.hs
@@ -0,0 +1,87 @@
+import System.IO
+
+whats :: [(String, [(String, String, String)])]
+whats = [("Unionable a => Signal a -> Signal a -> Signal a",
+          [
+            ("set", "=", "flip union"),
+            ("keep", ".", "union")
+          ]
+         ),
+         ("Num a => Signal a -> Signal a -> Signal a",
+          [
+            ("add", "+", "+"),
+           ("sub", "-", "-"),
+           ("mul", "*", "*")
+          ]
+         ),
+         ("Fractional a => Signal a -> Signal a -> Signal a",
+          [
+            ("div", "/", "/")
+          ]
+         ),
+         ("Integral a => Signal a -> Signal a -> Signal a",
+          [
+            ("mod", "%", "mod"),
+            ("pow", "^", "^")
+          ]
+         ),
+         ("Bits a => Signal a -> Signal a -> Signal a",
+          [
+            ("band", ".&.", ".&."),
+            ("bor", ".|.", ".|."),
+            ("bxor", ".^.", "xor")
+          ]
+         ),
+         ("Bits a => Signal a -> Signal Int -> Signal a",
+          [
+            ("bshiftl", ".<<.", "shiftL"),
+            ("bshiftr", ".>>.", "shiftR")
+          ]
+         ),
+         ("Ord a => Signal a -> Signal a -> Signal Bool",
+          [
+            ("lt", "<", "<"),
+            ("gt", ">", ">"),
+            ("lte", "<=", "<="),
+            ("gte", ">=", ">=")
+          ]
+         ),
+         ("Eq a => Signal a -> Signal a -> Signal Bool",
+          [
+            ("eq", "==", "=="),
+            ("ne", "/=", "/=")
+          ]
+         ),
+         ("Signal Bool -> Signal Bool -> Signal Bool",
+          [
+            ("and", "&&", "&&"),
+            ("or", ".||.", "||")
+          ]
+         )
+        ]
+
+hows :: [(String, (String -> String))]
+hows = [("Mix",      (\x -> "|" ++ x ++ "|")),
+        ("In",       ("|" ++)  ),
+        ("Out",      (++ "|")  ),
+        ("Squeeze",  ("||" ++) ),
+        ("Trig",     ("!" ++)  ),
+        ("TrigZero", ("!!" ++) )
+       ]
+
+fwhat (sig, ops) = concatMap fop ops
+  where fop (name, tidalop, haskellop) = "-- " ++ name ++ "\n\n" ++ concatMap fhow hows
+          where fhow (howname, howfix) = (name ++ howname ++ ", " ++ "(" ++ howfix tidalop ++ ")" ++ " :: " ++ sig ++ "\n"
+                                          ++ name ++ howname ++ " = op" ++ howname ++ " (" ++ haskellop ++ ")\n"
+                                          ++ "(" ++ howfix tidalop ++ ") = " ++ name ++ howname ++ "\n\n"
+                                         )
+  
+
+header :: IO ()
+header = do x <- openFile "composers-header.hs" ReadMode
+            y <- hGetContents x
+            putStr y
+
+main = do header
+          putStrLn $ concatMap fwhat whats
+
