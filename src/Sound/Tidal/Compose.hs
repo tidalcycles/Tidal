68d3ed82b9a7129e018ebdf1f145a33bf36f17ff
alex <alex@slab.org>
2022-09-16 10:19:10 +0100

    binary and logical operators

--- src/Sound/Tidal/Compose.hs
+++ src/Sound/Tidal/Compose.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE TemplateHaskell, FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Sound.Tidal.Compose where
 
 import Prelude hiding ((<*), (*>))
-import Language.Haskell.TH
 import Control.Monad (forM)
+import Data.Bits
 
 import qualified Data.Map.Strict as Map
 
@@ -49,36 +49,68 @@ opTrigZero f a b = trigZeroJoin $ fmap (\a -> fmap (\b -> f a b)  b) a
 
 -- set
 
-setMix, mix, (|<|) :: Unionable a => Signal a -> Signal a -> Signal a
-setMix = opMix union
+setMix, mix, (|=|) :: Unionable a => Signal a -> Signal a -> Signal a
+setMix = opMix (flip union)
 mix = setMix
-(|<|) = setMix
+(|=|) = setMix
 
-setIn, (|<) :: (Unionable a, Num a) => Signal a -> Signal a -> Signal a
-setIn = opIn union
-(|<) = setIn
+setIn, (|=) :: (Unionable a, Num a) => Signal a -> Signal a -> Signal a
+setIn = opIn (flip union)
+(|=) = setIn
 
-setOut, (<|) :: (Unionable a, Num a) => Signal a -> Signal a -> Signal a
-setOut = opOut union
-(<|) = setOut
+setOut, (=|) :: (Unionable a, Num a) => Signal a -> Signal a -> Signal a
+setOut = opOut (flip union)
+(=|) = setOut
 
-setSqueeze, (||<) :: (Unionable a, Num a) => Signal a -> Signal a -> Signal a
-setSqueeze = opSqueeze union
-(||<) = setSqueeze
+setSqueeze, (||=) :: (Unionable a, Num a) => Signal a -> Signal a -> Signal a
+setSqueeze = opSqueeze (flip union)
+(||=) = setSqueeze
 
-setSqueezeOut, (<||) :: (Unionable a, Num a) => Signal a -> Signal a -> Signal a
-setSqueezeOut = opSqueezeOut union
-(<||) = setSqueezeOut
+setSqueezeOut, (=||) :: (Unionable a, Num a) => Signal a -> Signal a -> Signal a
+setSqueezeOut = opSqueezeOut (flip union)
+(=||) = setSqueezeOut
 
-setTrig, (!<) :: (Unionable a, Num a) => Signal a -> Signal a -> Signal a
-setTrig = opTrig union
-(!<) = setTrig
+setTrig, (!=) :: (Unionable a, Num a) => Signal a -> Signal a -> Signal a
+setTrig = opTrig (flip union)
+(!=) = setTrig
 
-setTrigZero, (!!<) :: (Unionable a, Num a) => Signal a -> Signal a -> Signal a
-setTrigZero = opTrigZero union
-(!!<) = setTrig
+setTrigZero, (!!=) :: (Unionable a, Num a) => Signal a -> Signal a -> Signal a
+setTrigZero = opTrigZero (flip union)
+(!!=) = setTrig
 
-infix 4 |<|, |<, <|, ||<, <||, !<, !!<
+infix 4 |=|, |=, =|, ||=, =||, !=, !!=
+
+-- keep
+
+keepMix, (|.|) :: Unionable a => Signal a -> Signal a -> Signal a
+keepMix = opMix union
+(|.|) = keepMix
+
+keepIn, (|.) :: (Unionable a, Num a) => Signal a -> Signal a -> Signal a
+keepIn = opIn union
+(|.) = keepIn
+
+keepOut, (.|) :: (Unionable a, Num a) => Signal a -> Signal a -> Signal a
+keepOut = opOut union
+(.|) = keepOut
+
+keepSqueeze, (||.) :: (Unionable a, Num a) => Signal a -> Signal a -> Signal a
+keepSqueeze = opSqueeze union
+(||.) = keepSqueeze
+
+keepSqueezeOut, (.||) :: (Unionable a, Num a) => Signal a -> Signal a -> Signal a
+keepSqueezeOut = opSqueezeOut union
+(.||) = keepSqueezeOut
+
+keepTrig, (!.) :: (Unionable a, Num a) => Signal a -> Signal a -> Signal a
+keepTrig = opTrig union
+(!.) = keepTrig
+
+keepTrigZero, (!!.) :: (Unionable a, Num a) => Signal a -> Signal a -> Signal a
+keepTrigZero = opTrigZero union
+(!!.) = keepTrig
+
+infix 4 |.|, |., .|, ||., .||, !., !!.
 
 -- add
 
@@ -271,5 +303,423 @@ powTrigZero = opTrigZero (^)
 (!!^) = powTrig
 
 infix 4 |^|, |^, ^|, ||^, ^||, !^, !!^
+
+-- and (bitwise)
+
+bandMix, (|.&.|) :: Bits a => Signal a -> Signal a -> Signal a
+bandMix = opMix (.&.)
+(|.&.|) = bandMix
+
+bandIn, (|.&.) :: Bits a => Signal a -> Signal a -> Signal a
+bandIn = opIn (.&.)
+(|.&.) = bandIn
+
+bandOut, (.&.|) :: Bits a => Signal a -> Signal a -> Signal a
+bandOut = opOut (.&.)
+(.&.|) = bandOut
+
+bandSqueeze, (||.&.) :: Bits a => Signal a -> Signal a -> Signal a
+bandSqueeze = opSqueeze (.&.)
+(||.&.) = bandSqueeze
+
+bandSqueezeOut, (.&.||) :: Bits a => Signal a -> Signal a -> Signal a
+bandSqueezeOut = opSqueezeOut (.&.)
+(.&.||) = bandSqueezeOut
+
+bandTrig, (!.&.) :: Bits a => Signal a -> Signal a -> Signal a
+bandTrig = opTrig (.&.)
+(!.&.) = bandTrig
+
+bandTrigZero, (!!.&.) :: Bits a => Signal a -> Signal a -> Signal a
+bandTrigZero = opTrigZero (.&.)
+(!!.&.) = bandTrig
+
+infix 4 |.&.|, |.&., .&.|, ||.&., .&.||, !.&., !!.&.
+
+-- or (bitwise)
+
+borMix, (|.|.|) :: Bits a => Signal a -> Signal a -> Signal a
+borMix = opMix (.|.)
+(|.|.|) = borMix
+
+borIn, (|.|.) :: Bits a => Signal a -> Signal a -> Signal a
+borIn = opIn (.|.)
+(|.|.) = borIn
+
+borOut, (.|.|) :: Bits a => Signal a -> Signal a -> Signal a
+borOut = opOut (.|.)
+(.|.|) = borOut
+
+borSqueeze, (||.|.) :: Bits a => Signal a -> Signal a -> Signal a
+borSqueeze = opSqueeze (.|.)
+(||.|.) = borSqueeze
+
+borSqueezeOut, (.|.||) :: Bits a => Signal a -> Signal a -> Signal a
+borSqueezeOut = opSqueezeOut (.|.)
+(.|.||) = borSqueezeOut
+
+borTrig, (!.|.) :: Bits a => Signal a -> Signal a -> Signal a
+borTrig = opTrig (.|.)
+(!.|.) = borTrig
+
+borTrigZero, (!!.|.) :: Bits a => Signal a -> Signal a -> Signal a
+borTrigZero = opTrigZero (.|.)
+(!!.|.) = borTrig
+
+infix 4 |.|.|, |.|., .|.|, ||.|., .|.||, !.|., !!.|.
+
+-- xor
+
+xorMix, (|.^.|) :: Bits a => Signal a -> Signal a -> Signal a
+xorMix = opMix xor
+(|.^.|) = xorMix
+
+xorIn, (|.^.) :: Bits a => Signal a -> Signal a -> Signal a
+xorIn = opIn xor
+(|.^.) = xorIn
+
+xorOut, (.^.|) :: Bits a => Signal a -> Signal a -> Signal a
+xorOut = opOut xor
+(.^.|) = xorOut
+
+xorSqueeze, (||.^.) :: Bits a => Signal a -> Signal a -> Signal a
+xorSqueeze = opSqueeze xor
+(||.^.) = xorSqueeze
+
+xorSqueezeOut, (.^.||) :: Bits a => Signal a -> Signal a -> Signal a
+xorSqueezeOut = opSqueezeOut xor
+(.^.||) = xorSqueezeOut
+
+xorTrig, (!.^.) :: Bits a => Signal a -> Signal a -> Signal a
+xorTrig = opTrig xor
+(!.^.) = xorTrig
+
+xorTrigZero, (!!.^.) :: Bits a => Signal a -> Signal a -> Signal a
+xorTrigZero = opTrigZero xor
+(!!.^.) = xorTrig
+
+infix 4 |.^.|, |.^., .^.|, ||.^., .^.||, !.^., !!.^.
+
+-- lshift
+
+lshiftMix, (|.<<.|) :: Bits a => Signal a -> Signal Int -> Signal a
+lshiftMix = opMix shiftL
+(|.<<.|) = lshiftMix
+
+lshiftIn, (|.<<.) :: Bits a => Signal a -> Signal Int -> Signal a
+lshiftIn = opIn shiftL
+(|.<<.) = lshiftIn
+
+lshiftOut, (.<<.|) :: Bits a => Signal a -> Signal Int -> Signal a
+lshiftOut = opOut shiftL
+(.<<.|) = lshiftOut
+
+lshiftSqueeze, (||.<<.) :: Bits a => Signal a -> Signal Int -> Signal a
+lshiftSqueeze = opSqueeze shiftL
+(||.<<.) = lshiftSqueeze
+
+lshiftSqueezeOut, (.<<.||) :: Bits a => Signal a -> Signal Int -> Signal a
+lshiftSqueezeOut = opSqueezeOut shiftL
+(.<<.||) = lshiftSqueezeOut
+
+lshiftTrig, (!.<<.) :: Bits a => Signal a -> Signal Int -> Signal a
+lshiftTrig = opTrig shiftL
+(!.<<.) = lshiftTrig
+
+lshiftTrigZero, (!!.<<.) :: Bits a => Signal a -> Signal Int -> Signal a
+lshiftTrigZero = opTrigZero shiftL
+(!!.<<.) = lshiftTrig
+
+infix 4 |.<<.|, |.<<., .<<.|, ||.<<., .<<.||, !.<<., !!.<<.
+
+-- rshift
+
+rshiftMix, (|.>>.|) :: Bits a => Signal a -> Signal Int -> Signal a
+rshiftMix = opMix shiftR
+(|.>>.|) = rshiftMix
+
+rshiftIn, (|.>>.) :: Bits a => Signal a -> Signal Int -> Signal a
+rshiftIn = opIn shiftR
+(|.>>.) = rshiftIn
+
+rshiftOut, (.>>.|) :: Bits a => Signal a -> Signal Int -> Signal a
+rshiftOut = opOut shiftR
+(.>>.|) = rshiftOut
+
+rshiftSqueeze, (||.>>.) :: Bits a => Signal a -> Signal Int -> Signal a
+rshiftSqueeze = opSqueeze shiftR
+(||.>>.) = rshiftSqueeze
+
+rshiftSqueezeOut, (.>>.||) :: Bits a => Signal a -> Signal Int -> Signal a
+rshiftSqueezeOut = opSqueezeOut shiftR
+(.>>.||) = rshiftSqueezeOut
+
+rshiftTrig, (!.>>.) :: Bits a => Signal a -> Signal Int -> Signal a
+rshiftTrig = opTrig shiftR
+(!.>>.) = rshiftTrig
+
+rshiftTrigZero, (!!.>>.) :: Bits a => Signal a -> Signal Int -> Signal a
+rshiftTrigZero = opTrigZero shiftR
+(!!.>>.) = rshiftTrig
+
+infix 4 |.>>.|, |.>>., .>>.|, ||.>>., .>>.||, !.>>., !!.>>.
+
+-- lt
+
+ltMix, (|<|) :: Ord a => Signal a -> Signal a -> Signal Bool
+ltMix = opMix (<)
+(|<|) = ltMix
+
+ltIn, (|<) :: Ord a => Signal a -> Signal a -> Signal Bool
+ltIn = opIn (<)
+(|<) = ltIn
+
+ltOut, (<|) :: Ord a => Signal a -> Signal a -> Signal Bool
+ltOut = opOut (<)
+(<|) = ltOut
+
+ltSqueeze, (||<) :: Ord a => Signal a -> Signal a -> Signal Bool
+ltSqueeze = opSqueeze (<)
+(||<) = ltSqueeze
+
+ltSqueezeOut, (<||) :: Ord a => Signal a -> Signal a -> Signal Bool
+ltSqueezeOut = opSqueezeOut (<)
+(<||) = ltSqueezeOut
+
+ltTrig, (!<) :: Ord a => Signal a -> Signal a -> Signal Bool
+ltTrig = opTrig (<)
+(!<) = ltTrig
+
+ltTrigZero, (!!<) :: Ord a => Signal a -> Signal a -> Signal Bool
+ltTrigZero = opTrigZero (<)
+(!!<) = ltTrig
+
+infix 4 |<|, |<, <|, ||<, <||, !<, !!<
+
+-- lte
+
+lteMix, (|<=|) :: Ord a => Signal a -> Signal a -> Signal Bool
+lteMix = opMix (<=)
+(|<=|) = lteMix
+
+lteIn, (|<=) :: Ord a => Signal a -> Signal a -> Signal Bool
+lteIn = opIn (<=)
+(|<=) = lteIn
+
+lteOut, (<=|) :: Ord a => Signal a -> Signal a -> Signal Bool
+lteOut = opOut (<=)
+(<=|) = lteOut
+
+lteSqueeze, (||<=) :: Ord a => Signal a -> Signal a -> Signal Bool
+lteSqueeze = opSqueeze (<=)
+(||<=) = lteSqueeze
+
+lteSqueezeOut, (<=||) :: Ord a => Signal a -> Signal a -> Signal Bool
+lteSqueezeOut = opSqueezeOut (<=)
+(<=||) = lteSqueezeOut
+
+lteTrig, (!<=) :: Ord a => Signal a -> Signal a -> Signal Bool
+lteTrig = opTrig (<=)
+(!<=) = lteTrig
+
+lteTrigZero, (!!<=) :: Ord a => Signal a -> Signal a -> Signal Bool
+lteTrigZero = opTrigZero (<=)
+(!!<=) = lteTrig
+
+infix 4 |<=|, |<=, <=|, ||<=, <=||, !<=, !!<=
+
+-- gt
+
+gtMix, (|>|) :: Ord a => Signal a -> Signal a -> Signal Bool
+gtMix = opMix (>)
+(|>|) = gtMix
+
+gtIn, (|>) :: Ord a => Signal a -> Signal a -> Signal Bool
+gtIn = opIn (>)
+(|>) = gtIn
+
+gtOut, (>|) :: Ord a => Signal a -> Signal a -> Signal Bool
+gtOut = opOut (>)
+(>|) = gtOut
+
+gtSqueeze, (||>) :: Ord a => Signal a -> Signal a -> Signal Bool
+gtSqueeze = opSqueeze (>)
+(||>) = gtSqueeze
+
+gtSqueezeOut, (>||) :: Ord a => Signal a -> Signal a -> Signal Bool
+gtSqueezeOut = opSqueezeOut (>)
+(>||) = gtSqueezeOut
+
+gtTrig, (!>) :: Ord a => Signal a -> Signal a -> Signal Bool
+gtTrig = opTrig (>)
+(!>) = gtTrig
+
+gtTrigZero, (!!>) :: Ord a => Signal a -> Signal a -> Signal Bool
+gtTrigZero = opTrigZero (>)
+(!!>) = gtTrig
+
+infix 4 |>|, |>, >|, ||>, >||, !>, !!>
+
+-- gte
+
+gteMix, (|>=|) :: Ord a => Signal a -> Signal a -> Signal Bool
+gteMix = opMix (>=)
+(|>=|) = gteMix
+
+gteIn, (|>=) :: Ord a => Signal a -> Signal a -> Signal Bool
+gteIn = opIn (>=)
+(|>=) = gteIn
+
+gteOut, (>=|) :: Ord a => Signal a -> Signal a -> Signal Bool
+gteOut = opOut (>=)
+(>=|) = gteOut
+
+gteSqueeze, (||>=) :: Ord a => Signal a -> Signal a -> Signal Bool
+gteSqueeze = opSqueeze (>=)
+(||>=) = gteSqueeze
+
+gteSqueezeOut, (>=||) :: Ord a => Signal a -> Signal a -> Signal Bool
+gteSqueezeOut = opSqueezeOut (>=)
+(>=||) = gteSqueezeOut
+
+gteTrig, (!>=) :: Ord a => Signal a -> Signal a -> Signal Bool
+gteTrig = opTrig (>=)
+(!>=) = gteTrig
+
+gteTrigZero, (!!>=) :: Ord a => Signal a -> Signal a -> Signal Bool
+gteTrigZero = opTrigZero (>=)
+(!!>=) = gteTrig
+
+infix 4 |>=|, |>=, >=|, ||>=, >=||, !>=, !!>=
+
+-- eq
+
+eqMix, (|==|) :: Eq a => Signal a -> Signal a -> Signal Bool
+eqMix = opMix (==)
+(|==|) = eqMix
+
+eqIn, (|==) :: Eq a => Signal a -> Signal a -> Signal Bool
+eqIn = opIn (==)
+(|==) = eqIn
+
+eqOut, (==|) :: Eq a => Signal a -> Signal a -> Signal Bool
+eqOut = opOut (==)
+(==|) = eqOut
+
+eqSqueeze, (||==) :: Eq a => Signal a -> Signal a -> Signal Bool
+eqSqueeze = opSqueeze (==)
+(||==) = eqSqueeze
+
+eqSqueezeOut, (==||) :: Eq a => Signal a -> Signal a -> Signal Bool
+eqSqueezeOut = opSqueezeOut (==)
+(==||) = eqSqueezeOut
+
+eqTrig, (!==) :: Eq a => Signal a -> Signal a -> Signal Bool
+eqTrig = opTrig (==)
+(!==) = eqTrig
+
+eqTrigZero, (!!==) :: Eq a => Signal a -> Signal a -> Signal Bool
+eqTrigZero = opTrigZero (==)
+(!!==) = eqTrig
+
+infix 4 |==|, |==, ==|, ||==, ==||, !==, !!==
+
+-- ne
+-- TODO - define != aliases too
+  
+neMix, (|/=|) :: Eq a => Signal a -> Signal a -> Signal Bool
+neMix = opMix (/=)
+(|/=|) = neMix
+
+neIn, (|/=) :: Eq a => Signal a -> Signal a -> Signal Bool
+neIn = opIn (/=)
+(|/=) = neIn
+
+neOut, (/=|) :: Eq a => Signal a -> Signal a -> Signal Bool
+neOut = opOut (/=)
+(/=|) = neOut
+
+neSqueeze, (||/=) :: Eq a => Signal a -> Signal a -> Signal Bool
+neSqueeze = opSqueeze (/=)
+(||/=) = neSqueeze
+
+neSqueezeOut, (/=||) :: Eq a => Signal a -> Signal a -> Signal Bool
+neSqueezeOut = opSqueezeOut (/=)
+(/=||) = neSqueezeOut
+
+neTrig, (!/=) :: Eq a => Signal a -> Signal a -> Signal Bool
+neTrig = opTrig (/=)
+(!/=) = neTrig
+
+neTrigZero, (!!/=) :: Eq a => Signal a -> Signal a -> Signal Bool
+neTrigZero = opTrigZero (/=)
+(!!/=) = neTrig
+
+infix 4 |/=|, |/=, /=|, ||/=, /=||, !/=, !!/=
+
+-- and (non-bitwise)
+  
+andMix, (|&&|) :: Signal Bool -> Signal Bool -> Signal Bool
+andMix = opMix (&&)
+(|&&|) = andMix
+
+andIn, (|&&) :: Signal Bool -> Signal Bool -> Signal Bool
+andIn = opIn (&&)
+(|&&) = andIn
+
+andOut, (&&|) :: Signal Bool -> Signal Bool -> Signal Bool
+andOut = opOut (&&)
+(&&|) = andOut
+
+andSqueeze, (||&&) :: Signal Bool -> Signal Bool -> Signal Bool
+andSqueeze = opSqueeze (&&)
+(||&&) = andSqueeze
+
+andSqueezeOut, (&&||) :: Signal Bool -> Signal Bool -> Signal Bool
+andSqueezeOut = opSqueezeOut (&&)
+(&&||) = andSqueezeOut
+
+andTrig, (!&&) :: Signal Bool -> Signal Bool -> Signal Bool
+andTrig = opTrig (&&)
+(!&&) = andTrig
+
+andTrigZero, (!!&&) :: Signal Bool -> Signal Bool -> Signal Bool
+andTrigZero = opTrigZero (&&)
+(!!&&) = andTrig
+
+infix 4 |&&|, |&&, &&|, ||&&, &&||, !&&, !!&&
+
+-- or (non-bitwise)
+-- TODO oh dear..
+
+orMix, (|.||.|) :: Signal Bool -> Signal Bool -> Signal Bool
+orMix = opMix (||)
+(|.||.|) = orMix
+
+orIn, (|.||.) :: Signal Bool -> Signal Bool -> Signal Bool
+orIn = opIn (||)
+(|.||.) = orIn
+
+orOut, (.||.|) :: Signal Bool -> Signal Bool -> Signal Bool
+orOut = opOut (||)
+(.||.|) = orOut
+
+orSqueeze, (||.||.) :: Signal Bool -> Signal Bool -> Signal Bool
+orSqueeze = opSqueeze (||)
+(||.||.) = orSqueeze
+
+orSqueezeOut, (.||.||) :: Signal Bool -> Signal Bool -> Signal Bool
+orSqueezeOut = opSqueezeOut (||)
+(.||.||) = orSqueezeOut
+
+orTrig, (!.||.) :: Signal Bool -> Signal Bool -> Signal Bool
+orTrig = opTrig (||)
+(!.||.) = orTrig
+
+orTrigZero, (!!.||.) :: Signal Bool -> Signal Bool -> Signal Bool
+orTrigZero = opTrigZero (||)
+(!!.||.) = orTrig
+
+infix 4 |.||.|, |.||., .||.|, ||.||., .||.||, !.||., !!.||.
 
 -- ************************************************************ --
