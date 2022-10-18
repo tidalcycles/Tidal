b17ea7322376bb5a280e5acd2782769438c7b921
alex <alex@slab.org>
2022-10-18 22:25:57 +0100

    port more tests

--- test/Test.hs
+++ test/Test.hs
@@ -7,11 +7,9 @@ import Test.Microspec
 -- import Sound.Tidal.ParseTest
 -- import Sound.Tidal.PatternTest
 -- import Sound.Tidal.ControlTest
--- import Sound.Tidal.ScalesTest
 -- import Sound.Tidal.ChordsTest
 -- import Sound.Tidal.StreamTest
 -- import Sound.Tidal.UITest
--- import Sound.Tidal.UtilsTest
 -- import Sound.Tidal.ExceptionsTest
 import Sound.Tidal.ArcTest
 import Sound.Tidal.ChordsTest
@@ -19,10 +17,12 @@ import Sound.Tidal.EventTest
 import Sound.Tidal.ExceptionsTest
 import Sound.Tidal.ParamsTest
 import Sound.Tidal.ParseBPTest
+import Sound.Tidal.ScalesTest
 import Sound.Tidal.SignalBaseTest
 import Sound.Tidal.StreamTest
 import Sound.Tidal.TimeTest
 import Sound.Tidal.TypesTest
+import Sound.Tidal.UtilsTest
 import Sound.Tidal.ValueTest
 import Sound.Tidal.WaveformTest
 
@@ -33,11 +33,9 @@ main = microspec $ do
 --  Sound.Tidal.ParamsTest.run
 --  Sound.Tidal.PatternTest.run
 --  Sound.Tidal.ControlTest.run
---  Sound.Tidal.ScalesTest.run
 --  Sound.Tidal.ChordsTest.run
 --  Sound.Tidal.StreamTest.run
 --  Sound.Tidal.UITest.run
---  Sound.Tidal.UtilsTest.run
 --  Sound.Tidal.ExceptionsTest.run
   Sound.Tidal.ArcTest.run
   Sound.Tidal.ChordsTest.run
@@ -45,9 +43,11 @@ main = microspec $ do
   Sound.Tidal.ExceptionsTest.run
   Sound.Tidal.ParamsTest.run
   Sound.Tidal.ParseBPTest.run
+  Sound.Tidal.ScalesTest.run
   Sound.Tidal.SignalBaseTest.run
   Sound.Tidal.StreamTest.run
   Sound.Tidal.TimeTest.run
   Sound.Tidal.TypesTest.run
+  Sound.Tidal.UtilsTest.run
   Sound.Tidal.ValueTest.run
   Sound.Tidal.WaveformTest.run
