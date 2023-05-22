8d5278e76f3081b42265a35ff19ff5f59cbcf3e3
Alex McLean <alex@slab.org>
2023-05-22 12:54:51 +0100

    more tests

--- test/Sound/Tidal/SequenceTest.hs
+++ test/Sound/Tidal/SequenceTest.hs
@@ -28,3 +28,19 @@ run =
                 Atom 0.5 0.5 0 $ Just (10,2),
                 Atom 0.5 0 0 Nothing
                ] :: Sequence (Int,Int))
+    describe "alignF" $ do
+      it "Can align and combine two sequences by Expansion and addition" $ do
+        ((alignF Expand In (+) "0 1 2" "10 20") :: Sequence Int)
+          `shouldBe`
+          (Cat [Atom 1 0 0 $ Just 10, Atom 0.5 0 0.5 $ Just 11, Atom 0.5 0.5 0 $ Just 21,
+                Atom 1 0 0 $ Just 22])
+      it "Can align and combine subsequences by Expansion and addition" $ do
+        ((alignF Expand In (+) "0 [1 2] 3" "10 20") :: Sequence Int)
+          `shouldBe`
+          (Cat [Atom 1 0 0 $ Just 10, Atom 0.5 0 0 $ Just 11, Atom 0.5 0 0 $ Just 22,
+                Atom 1 0 0 $ Just 23])
+      it "Can align and combine subsequences by Expansion and addition" $ do
+        ((alignF Expand In (+) "0 [1 2] 3" "10 [20 30]") :: Sequence Int)
+          `shouldBe`
+          (Cat [Atom 1 0 0 $ Just 10, Atom 0.5 0 0 $ Just 11, Atom 0.5 0 0 $ Just 22,
+                Atom 0.25 0 0.75 $ Just 23, Atom 0.75 0.25 0 $ Just 33])
