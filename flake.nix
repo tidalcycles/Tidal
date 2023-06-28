1deeb70589cb1aceb1d33b3fdd374af0071534dd
mitchmindtree <mail@mitchellnordine.com>
2023-06-28 18:53:45 +1000

    Fix nix build for Tidal 2.0 (include hosc 0.20). Update nixpkgs input.
    This resolves the nix build issue encountered at
    https://github.com/tidalcycles/Tidal/pull/1022, mentioned in
    https://github.com/tidalcycles/Tidal/pull/1022#issuecomment-1608125266.
    
    Namely, as Tidal 2.0 requires `hosc` `0.20` but nixpkgs currently only
    provides `0.19.1`, we temporarily include `hosc` `0.20` directly from
    its source.
    
    This commit also updates the nixpkgs pin to the latest version while
    we're at it.

--- flake.nix
+++ flake.nix
@@ -23,6 +23,14 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Temporarily add `hosc` at the latest v0.20 commit
+    # (nixpkgs currently only has 0.19.1).
+    # See this comment for details:
+    # https://github.com/tidalcycles/Tidal/pull/1022#issuecomment-1610978403
+    hosc = {
+      url = "github:rd--/hosc?rev=e77aa67cd0b99a32498fef246a687ba443c9b4be";
+      flake = false;
+    };
   };
 
   outputs = inputs: let
@@ -38,6 +46,7 @@
 
     mkPackages = pkgs: let
       project = pkgs.haskellPackages.extend (pkgs.haskell.lib.compose.packageSourceOverrides {
+        hosc = inputs.hosc; # Remove once `hosc` is at 0.20 in nixpkgs.
         tidal = ./.;
         tidal-link = ./tidal-link;
         tidal-listener = ./tidal-listener;
