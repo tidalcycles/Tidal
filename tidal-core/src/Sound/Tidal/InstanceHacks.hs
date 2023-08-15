ee9919df280d7491410002f29c4abec0418bf77e
Alex McLean <alex@slab.org>
2023-08-15 18:14:36 +0100

    add missing files

---
+++ tidal-core/src/Sound/Tidal/InstanceHacks.hs
@@ -0,0 +1,191 @@
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+module Sound.Tidal.InstanceHacks where
+
+import           Control.Applicative  (liftA2)
+import qualified Data.Map.Strict      as Map
+import           Sound.Tidal.Sequence ()
+import           Sound.Tidal.Signal   ()
+import           Sound.Tidal.Types
+import           Sound.Tidal.Value
+
+noOv :: String -> a
+noOv meth = error $ meth ++ ": not supported for patterns"
+
+-- ************************************************************ --
+-- ValueMaps as numbers
+
+instance Num ValueMap where
+  negate      = (applyFIRS negate negate negate id <$>)
+  (+)         = Map.unionWith (fNum2 (+) (+))
+  (*)         = Map.unionWith (fNum2 (*) (*))
+  fromInteger i = Map.singleton "n" $ VI (fromInteger i)
+  signum      = (applyFIRS signum signum signum id <$>)
+  abs         = (applyFIRS abs abs abs id <$>)
+
+instance Fractional ValueMap where
+  recip        = fmap (applyFIRS recip id recip id)
+  fromRational r = Map.singleton "speed" $ VF (fromRational r)
+
+-- ************************************************************ --
+-- Signals as numbers
+
+instance Eq (Signal a) where
+  (==) = noOv "(==)"
+
+instance Ord a => Ord (Signal a) where
+  min = liftA2 min
+  max = liftA2 max
+  compare = noOv "compare"
+  (<=) = noOv "(<=)"
+
+instance Num a => Num (Signal a) where
+  negate      = fmap negate
+  (+)         = liftA2 (+)
+  (*)         = liftA2 (*)
+  fromInteger = pure . fromInteger
+  abs         = fmap abs
+  signum      = fmap signum
+
+instance Enum a => Enum (Signal a) where
+  succ           = fmap succ
+  pred           = fmap pred
+  toEnum         = pure . toEnum
+  fromEnum       = noOv "fromEnum"
+  enumFrom       = noOv "enumFrom"
+  enumFromThen   = noOv "enumFromThen"
+  enumFromTo     = noOv "enumFromTo"
+  enumFromThenTo = noOv "enumFromThenTo"
+
+instance (Num a, Ord a) => Real (Signal a) where
+  toRational = noOv "toRational"
+
+instance (Integral a) => Integral (Signal a) where
+  quot          = liftA2 quot
+  rem           = liftA2 rem
+  div           = liftA2 div
+  mod           = liftA2 mod
+  toInteger     = noOv "toInteger"
+  x `quotRem` y = (x `quot` y, x `rem` y)
+  x `divMod`  y = (x `div`  y, x `mod` y)
+
+instance (Fractional a) => Fractional (Signal a) where
+  recip        = fmap recip
+  fromRational = pure . fromRational
+
+instance (Floating a) => Floating (Signal a) where
+  pi    = pure pi
+  sqrt  = fmap sqrt
+  exp   = fmap exp
+  log   = fmap log
+  sin   = fmap sin
+  cos   = fmap cos
+  asin  = fmap asin
+  atan  = fmap atan
+  acos  = fmap acos
+  sinh  = fmap sinh
+  cosh  = fmap cosh
+  asinh = fmap asinh
+  atanh = fmap atanh
+  acosh = fmap acosh
+
+instance (RealFrac a) => RealFrac (Signal a) where
+  properFraction = noOv "properFraction"
+  truncate       = noOv "truncate"
+  round          = noOv "round"
+  ceiling        = noOv "ceiling"
+  floor          = noOv "floor"
+
+instance (RealFloat a) => RealFloat (Signal a) where
+  floatRadix     = noOv "floatRadix"
+  floatDigits    = noOv "floatDigits"
+  floatRange     = noOv "floatRange"
+  decodeFloat    = noOv "decodeFloat"
+  encodeFloat    = ((.).(.)) pure encodeFloat
+  exponent       = noOv "exponent"
+  significand    = noOv "significand"
+  scaleFloat n   = fmap (scaleFloat n)
+  isNaN          = noOv "isNaN"
+  isInfinite     = noOv "isInfinite"
+  isDenormalized = noOv "isDenormalized"
+  isNegativeZero = noOv "isNegativeZero"
+  isIEEE         = noOv "isIEEE"
+  atan2          = liftA2 atan2
+
+-- ************************************************************ --
+-- Sequences as numbers
+
+instance Num a => Num (Sequence a) where
+  negate      = fmap negate
+  (+)         = liftA2 (+)
+  (*)         = liftA2 (*)
+  fromInteger = pure . fromInteger
+  abs         = fmap abs
+  signum      = fmap signum
+
+instance Enum a => Enum (Sequence a) where
+  succ           = fmap succ
+  pred           = fmap pred
+  toEnum         = pure . toEnum
+  fromEnum       = noOv "fromEnum"
+  enumFrom       = noOv "enumFrom"
+  enumFromThen   = noOv "enumFromThen"
+  enumFromTo     = noOv "enumFromTo"
+  enumFromThenTo = noOv "enumFromThenTo"
+
+instance (Num a, Ord a) => Real (Sequence a) where
+  toRational = noOv "toRational"
+
+instance (Integral a) => Integral (Sequence a) where
+  quot          = liftA2 quot
+  rem           = liftA2 rem
+  div           = liftA2 div
+  mod           = liftA2 mod
+  toInteger     = noOv "toInteger"
+  x `quotRem` y = (x `quot` y, x `rem` y)
+  x `divMod`  y = (x `div`  y, x `mod` y)
+
+instance (Fractional a) => Fractional (Sequence a) where
+  recip        = fmap recip
+  fromRational = pure . fromRational
+
+instance (Floating a) => Floating (Sequence a) where
+  pi    = pure pi
+  sqrt  = fmap sqrt
+  exp   = fmap exp
+  log   = fmap log
+  sin   = fmap sin
+  cos   = fmap cos
+  asin  = fmap asin
+  atan  = fmap atan
+  acos  = fmap acos
+  sinh  = fmap sinh
+  cosh  = fmap cosh
+  asinh = fmap asinh
+  atanh = fmap atanh
+  acosh = fmap acosh
+
+instance (RealFrac a) => RealFrac (Sequence a) where
+  properFraction = noOv "properFraction"
+  truncate       = noOv "truncate"
+  round          = noOv "round"
+  ceiling        = noOv "ceiling"
+  floor          = noOv "floor"
+
+instance (RealFloat a) => RealFloat (Sequence a) where
+  floatRadix     = noOv "floatRadix"
+  floatDigits    = noOv "floatDigits"
+  floatRange     = noOv "floatRange"
+  decodeFloat    = noOv "decodeFloat"
+  encodeFloat    = ((.).(.)) pure encodeFloat
+  exponent       = noOv "exponent"
+  significand    = noOv "significand"
+  scaleFloat n   = fmap (scaleFloat n)
+  isNaN          = noOv "isNaN"
+  isInfinite     = noOv "isInfinite"
+  isDenormalized = noOv "isDenormalized"
+  isNegativeZero = noOv "isNegativeZero"
+  isIEEE         = noOv "isIEEE"
+  atan2          = liftA2 atan2
+
---
+++ tidal-core/src/Sound/Tidal/Value.hs
@@ -0,0 +1,71 @@
+-- (c) Alex McLean 2023
+-- Shared under the terms of the GNU Public License v. 3.0
+
+module Sound.Tidal.Value where
+
+import           Data.Word         (Word8)
+import           Sound.Tidal.Types
+
+-- | Apply one of three functions to a Value, depending on its type
+applyFIRS :: (Double -> Double) -> (Int -> Int) -> (Rational -> Rational) -> (String -> String) -> Value -> Value
+applyFIRS f _ _ _ (VF f')        = VF (f f')
+applyFIRS f _ _ _ (VN (Note f')) = VN (Note $ f f')
+applyFIRS _ f _ _ (VI i)         = VI (f i)
+applyFIRS _ _ f _ (VR i)         = VR (f i)
+applyFIRS _ _ _ f (VS s)         = VS (f s)
+-- applyFIS f f' f'' (VState x) = VState $ \cmap -> (applyFIS f f' f'') <$> (x cmap)
+applyFIRS _ _ _ _ v              = v
+
+-- | Apply one of two functions to a pair of Values, depending on
+-- their types (int or float; strings and rationals are ignored)
+fNum2 :: (Int -> Int -> Int) -> (Double -> Double -> Double) -> Value -> Value -> Value
+fNum2 fInt _      (VI a) (VI b)               = VI (fInt a b)
+fNum2 _    fFloat (VF a) (VF b)               = VF (fFloat a b)
+fNum2 _    fFloat (VN (Note a)) (VN (Note b)) = VN (Note $ fFloat a b)
+fNum2 _    fFloat (VF a) (VN (Note b))        = VN (Note $ fFloat a b)
+fNum2 _    fFloat (VN (Note a)) (VF b)        = VN (Note $ fFloat a b)
+fNum2 _    fFloat (VI a) (VF b)               = VF (fFloat (fromIntegral a) b)
+fNum2 _    fFloat (VF a) (VI b)               = VF (fFloat a (fromIntegral b))
+-- fNum2 fInt fFloat (VState a) b = VState $ \cmap -> ((\a' -> fNum2 fInt fFloat a' b) <$> (a cmap))
+-- fNum2 fInt fFloat a (VState b) = VState $ \cmap -> ((\b' -> fNum2 fInt fFloat a b') <$> (b cmap))
+fNum2 _    _      x      _                    = x
+
+getI :: Value -> Maybe Int
+getI (VI i) = Just i
+getI (VR x) = Just $ floor x
+getI (VF x) = Just $ floor x
+getI _      = Nothing
+
+getF :: Value -> Maybe Double
+getF (VF f) = Just f
+getF (VR x) = Just $ fromRational x
+getF (VI x) = Just $ fromIntegral x
+getF _      = Nothing
+
+getN :: Value -> Maybe Note
+getN (VF f) = Just $ Note f
+getN (VR x) = Just $ Note $ fromRational x
+getN (VI x) = Just $ Note $ fromIntegral x
+getN _      = Nothing
+
+getS :: Value -> Maybe String
+getS (VS s) = Just s
+getS _      = Nothing
+
+getB :: Value -> Maybe Bool
+getB (VB b) = Just b
+getB _      = Nothing
+
+getR :: Value -> Maybe Rational
+getR (VR r) = Just r
+getR (VF x) = Just $ toRational x
+getR (VI x) = Just $ toRational x
+getR _      = Nothing
+
+getBlob :: Value -> Maybe [Word8]
+getBlob (VX xs) = Just xs
+getBlob _       = Nothing
+
+getList :: Value -> Maybe [Value]
+getList (VList vs) = Just vs
+getList _          = Nothing
