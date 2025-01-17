50b443e8aff4bf6b8cea439a5ba2641c6ca300f2
alex <alex@slab.org>
2025-01-17 20:41:26 +0000

    fix off-by-one error in striateBy, and make it work within any existing begin/end controls

--- src/Sound/Tidal/Control.hs
+++ src/Sound/Tidal/Control.hs
@@ -165,9 +165,16 @@ striate' :: Pattern Int -> Pattern Double -> ControlPattern -> ControlPattern
 striate' = striateBy
 
 _striateBy :: Int -> Double -> ControlPattern -> ControlPattern
-_striateBy n f p = fastcat $ map (offset . fromIntegral) [0 .. n-1]
-  where offset i = p # P.begin (pure (slot * i) :: Pattern Double) # P.end (pure ((slot * i) + f) :: Pattern Double)
-        slot = (1 - f) / fromIntegral n
+-- _striateBy n f p = fastcat $ map (offset . fromIntegral) [0 .. n-1]
+--   where offset i = p # P.begin (pure (slot * i) :: Pattern Double) # P.end (pure ((slot * i) + f) :: Pattern Double)
+--         slot = (1 - f) / fromIntegral n
+-- _striateBy :: Int -> Double -> ControlPattern -> ControlPattern
+-- _striateBy n f p = fastcat $ map (offset) [0 .. n-1]
+--   where offset i = p # P.begin (pure (slot * i) :: Pattern Double) # P.end (pure ((slot * i) + f) :: Pattern Double)
+--         slot = (1 - f) / fromIntegral (n-1)
+_striateBy n f p = keepTactus (withTactus (* toRational n) p) $ fastcat $ map (offset . fromIntegral) [0 .. n-1]
+  where offset i = mergePlayRange (slot*i, (slot*i)+f) <$> p
+        slot = (1 - f) / fromIntegral (n-1)
 
 
 {- | `gap` is similar to `chop` in that it granualizes every sample in place as it is played,
